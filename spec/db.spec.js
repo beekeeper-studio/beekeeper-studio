70fc53da363fcfe184b882350e738b440feafcb5
Max Claus Nunes <maxcnunes@gmail.com>
2015-12-08 21:13:32 -0200

    Move select query tests to a specific “describe” block
    In the next commits I’m going to create more tests for: insert, delete and update. So this is better for the organization.

--- spec/db.spec.js
+++ spec/db.spec.js
@@ -78,40 +78,42 @@ describe('db', () => {
             ]);
           });
 
-          it('should execute a single select query', async () => {
-            const result = await db.executeQuery(`select * from ${wrapQuery('users')}`);
-            expect(result).to.have.deep.property('fields[0].name').to.eql('id');
-            expect(result).to.have.deep.property('fields[1].name').to.eql('username');
-            expect(result).to.have.deep.property('fields[2].name').to.eql('email');
-            expect(result).to.have.deep.property('fields[3].name').to.eql('password');
+          describe('select query', () => {
+            it('should execute a single query', async () => {
+              const result = await db.executeQuery(`select * from ${wrapQuery('users')}`);
+              expect(result).to.have.deep.property('fields[0].name').to.eql('id');
+              expect(result).to.have.deep.property('fields[1].name').to.eql('username');
+              expect(result).to.have.deep.property('fields[2].name').to.eql('email');
+              expect(result).to.have.deep.property('fields[3].name').to.eql('password');
 
-            expect(result).to.have.deep.property('rows[0].id').to.eql(1);
-            expect(result).to.have.deep.property('rows[0].username').to.eql('maxcnunes');
-            expect(result).to.have.deep.property('rows[0].password').to.eql('123456');
-            expect(result).to.have.deep.property('rows[0].email').to.eql('maxcnunes@gmail.com');
-          });
+              expect(result).to.have.deep.property('rows[0].id').to.eql(1);
+              expect(result).to.have.deep.property('rows[0].username').to.eql('maxcnunes');
+              expect(result).to.have.deep.property('rows[0].password').to.eql('123456');
+              expect(result).to.have.deep.property('rows[0].email').to.eql('maxcnunes@gmail.com');
+            });
 
-          it('should execute multiple select queries', async () => {
-            const result = await db.executeQuery(`
-              select * from ${wrapQuery('users')};
-              select * from ${wrapQuery('roles')};
-            `);
+            it('should execute multiple queries', async () => {
+              const result = await db.executeQuery(`
+                select * from ${wrapQuery('users')};
+                select * from ${wrapQuery('roles')};
+              `);
 
-            expect(result).to.have.deep.property('fields[0][0].name').to.eql('id');
-            expect(result).to.have.deep.property('fields[0][1].name').to.eql('username');
-            expect(result).to.have.deep.property('fields[0][2].name').to.eql('email');
-            expect(result).to.have.deep.property('fields[0][3].name').to.eql('password');
+              expect(result).to.have.deep.property('fields[0][0].name').to.eql('id');
+              expect(result).to.have.deep.property('fields[0][1].name').to.eql('username');
+              expect(result).to.have.deep.property('fields[0][2].name').to.eql('email');
+              expect(result).to.have.deep.property('fields[0][3].name').to.eql('password');
 
-            expect(result).to.have.deep.property('rows[0][0].id').to.eql(1);
-            expect(result).to.have.deep.property('rows[0][0].username').to.eql('maxcnunes');
-            expect(result).to.have.deep.property('rows[0][0].password').to.eql('123456');
-            expect(result).to.have.deep.property('rows[0][0].email').to.eql('maxcnunes@gmail.com');
+              expect(result).to.have.deep.property('rows[0][0].id').to.eql(1);
+              expect(result).to.have.deep.property('rows[0][0].username').to.eql('maxcnunes');
+              expect(result).to.have.deep.property('rows[0][0].password').to.eql('123456');
+              expect(result).to.have.deep.property('rows[0][0].email').to.eql('maxcnunes@gmail.com');
 
-            expect(result).to.have.deep.property('fields[1][0].name').to.eql('id');
-            expect(result).to.have.deep.property('fields[1][1].name').to.eql('name');
+              expect(result).to.have.deep.property('fields[1][0].name').to.eql('id');
+              expect(result).to.have.deep.property('fields[1][1].name').to.eql('name');
 
-            expect(result).to.have.deep.property('rows[1][0].id').to.eql(1);
-            expect(result).to.have.deep.property('rows[1][0].name').to.eql('developer');
+              expect(result).to.have.deep.property('rows[1][0].id').to.eql(1);
+              expect(result).to.have.deep.property('rows[1][0].name').to.eql('developer');
+            });
           });
         });
       });
