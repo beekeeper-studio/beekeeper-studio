86152909e5f9c093b0f6ee465684301dcdccbdba
Max Claus Nunes <maxcnunes@gmail.com>
2017-05-21 22:28:28 -0300

    Add crypto validation

--- src/crypto.js
+++ src/crypto.js
@@ -4,6 +4,10 @@ import crypto from 'crypto';
 const algorithm = 'aes-256-ctr';
 
 export function encrypt(text, secret) {
+  if (!secret) {
+    throw new Error('Missing crypto secret');
+  }
+
   const cipher = crypto.createCipher(algorithm, secret);
   let crypted = cipher.update(text, 'utf8', 'hex');
   crypted += cipher.final('hex');
@@ -11,6 +15,10 @@ export function encrypt(text, secret) {
 }
 
 export function decrypt(text, secret) {
+  if (!secret) {
+    throw new Error('Missing crypto secret');
+  }
+
   const decipher = crypto.createDecipher(algorithm, secret);
   let dec = decipher.update(text, 'hex', 'utf8');
   dec += decipher.final('utf8');
