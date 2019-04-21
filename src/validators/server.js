3d2b79d66571ea6916e26a6711eb9333edfefe5c
Matthew Rathbone <matthew@foursquare.com>
2019-04-21 03:14:38 +0000

    lint issue

--- src/validators/server.js
+++ src/validators/server.js
@@ -5,14 +5,14 @@ import { CLIENTS } from '../db';
 function serverAddressValidator(ctx) {
   const { host, port, socketPath } = ctx.obj;
 
-  if(!host && !socketPath) {
+  if (!host && !socketPath) {
     return {
       validator: 'serverAddressValidator',
       msg: 'You must use host+port or socket path',
     };
   }
 
-  if(host && socketPath) {
+  if (host && socketPath) {
     return {
       validator: 'serverAddressValidator',
       msg: 'You must use host+port or socket path',
