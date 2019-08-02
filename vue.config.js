ba2c9a1af13d1181777a5bbb57d44d2aea9f4932
Matthew Rathbone <matthew@foursquare.com>
2019-08-02 14:51:33 +0000

    finally got it booting

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,14 @@
+
+const externals = ['sqlite3', 'sequelize', 'mysql']
 module.exports = {
+  pluginOptions: {
+    electronBuilder: {
+      externals
+    }
+  },
   configureWebpack: {
     plugins: [
-    ]
-
+    ],
+    // externals
   }
 }
