f25caa707f4a11e27e82dc30df5a81302dc3149e
Matthew Rathbone <matthew@foursquare.com>
2019-08-06 15:13:09 +0000

    connection interface update

--- src/components/ConnectionInterface.vue
+++ src/components/ConnectionInterface.vue
@@ -1,62 +1,90 @@
 <template>
-  <div>
-    <div class="container">
-      <div class="row justify-content-sm-center">
-        <div class="col-md-6">
-          <div class="card mt-5">
-            <div class="card-body">
-              <h5 class="card-title">Enter Connection Information</h5>
-              <form @action="submit">
-                <div class="form-group">
-                  <label for="connectionType">Connection Type</label>
-                  <select name="connectionType" class="form-control custom-select" v-model="connectionType" id="connection-select">
-                    <option value="mysql">MySQL</option>
-                    <option value="psql">Postgres</option>
-                  </select>
-                </div>
-                <div class="form-group">
-                  <label for="Host">Host</label>
-                  <input type="text" class="form-control" name="host" v-model="host">
-                </div>
+  <div class="connection-interface">
+    <!-- TODO: move to a component -->
+    <div class="connection-sidebar">
+      <div id="sidebar-wrapper" class="bg-light border-right">
+        <h5 class="p-3">Saved Connections</h5>
+        <!-- <div class="sidbar-heading mt-2 p-3">Saved Connections</div> -->
+        <div class="list-group list-group-flush">
+          <a href="#" class="list-group-item-link list-group-item">
+            Foo
+            <span class="float-right"><a href="" class="">CONNECT</a></span>
+          </a>
+        </div>
+      </div>
+    </div>
+    <!-- END TODO -->
+    <div class="connection-main">
+      <div class="container">
+        <div class="row justify-content-sm-center">
+          <div class="col-md-6">
+            <div class="card mt-5">
+              <div class="card-body">
+                <h5 class="card-title">Enter Connection Information</h5>
+                <form @action="submit">
+                  <div class="form-group">
+                    <label for="connectionType">Connection Type</label>
+                    <select name="connectionType" class="form-control custom-select" v-model="config.connectionType" id="connection-select">
+                      <option :key="t.value" v-for="t in connectionTypes" value="t.value">{{t.name}}</option>
+                    </select>
+                  </div>
+                  <div class="form-group">
+                    <label for="Host">Host</label>
+                    <input type="text" class="form-control" name="host" v-model="config.host">
+                  </div>
+                  <div class="form-group">
+                    <label for="port">Port</label>
+                    <input type="number" class="form-control" name="port" v-model="config.port">
+                  </div>
 
-                <div class="text-right">
-                  <button class="btn btn-primary mr-2" @click="saveConnection">Save</button>
-                  <button class="btn btn-success mr-2" @click="testConnection">Test</button>
-                  <button class="btn btn-info" @click="submit">Connect</button>
-                </div>
-              </form>
+                  <div class="text-right">
+                    <button class="btn btn-primary mr-2" @click="saveConnection">Save</button>
+                    <button class="btn btn-success mr-2" @click="testConnection">Test</button>
+                    <button class="btn btn-info" @click="submit">Connect</button>
+                  </div>
+                </form>
+              </div>
+
             </div>
 
           </div>
-
         </div>
-      </div>
-      <div class="row justify-content-sm-center mt-5">
-        <div class="col-md-6">
-          <div class="card">
-            <div class="card-body">
-              <h5 class="card-title">Choose a saved connection</h5>
-            </div>
-            <div class="list-group">
-              <a href="#" class="list-group-item-link list-group-item">
-                Foo
-                <span class="float-right"><a href="" class="">CONNECT</a></span>
-              </a>
+        <div class="row justify-content-sm-center mt-5">
+          <div class="col-md-6">
+            <div class="card">
+              <div class="card-body">
+                <h5 class="card-title">Recent Connections</h5>
+              </div>
+
             </div>
           </div>
         </div>
       </div>
     </div>
+
   </div>
 
 </template>
 
 <script>
+
+  import ConnectionConfig from '../models/connection-config'
+  import _ from 'lodash'
+
   export default {
     data() {
       return {
-        host: 'localhost',
-        connectionType: 'mysql'
+        config: ConnectionConfig.build({})
+      }
+    },
+    computed: {
+      connectionTypes() {
+        return _.map(ConnectionConfig.rawAttributes.connectionType.values, (t) => {
+          return {
+            value: t,
+            name: _.upperFirst(t)
+          }
+        })
       }
     }
   }
--- src/models/connection-config.js
+++ src/models/connection-config.js
@@ -1,26 +1,41 @@
 import Sequelize from 'sequelize'
 import sequelize from '../sequelize-db'
 
-const ConnectionConfig = sequelize.define('connection_config', {
-  connectionType: {
-    type: Sequelize.ENUM,
-    values: ['postgres', 'mysql']
+const ConnectionConfig = sequelize.define('connection_config',
+  {
+    connectionType: {
+      type: Sequelize.ENUM,
+      values: ['postgres', 'mysql'],
+      defaultValue: 'mysql'
+    },
+    host: {
+      type: Sequelize.STRING
+    },
+    port: {
+      type: Sequelize.INTEGER
+    },
+    defaultDatabase: {
+      type: Sequelize.STRING
+    },
+    user: {
+      type: Sequelize.STRING
+    },
+    password: {
+      type: Sequelize.STRING
+    }
   },
-  host: {
-    type: Sequelize.STRING
-  },
-  port: {
-    type: Sequelize.INTEGER
-  },
-  defaultDatabase: {
-    type: Sequelize.STRING
-  },
-  user: {
-    type: Sequelize.STRING
-  },
-  password: {
-    type: Sequelize.STRING
+  // END OF COLUMNS
+  {
+    instanceMethods: {
+      setDefaultsFor(cType) {
+        if(cType === 'mysql') {
+          this.host = this.host || 'localhost'
+        }
+      }
+    }
   }
-})
+
+
+)
 
 export default ConnectionConfig
