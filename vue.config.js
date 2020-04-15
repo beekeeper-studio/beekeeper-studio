871e16ba3a031cb855c1575f1e7d8597da18a058
Matthew Rathbone <matthew.rathbone@gmail.com>
2020-04-15 16:34:44 -0500

    bump for publish test

--- vue.config.js
+++ vue.config.js
@@ -33,7 +33,7 @@ module.exports = {
               owner: 'beekeeper-studio'
             },
             {
-              provider: 'snap',
+              provider: 'snapStore',
               channels: ['edge', 'stable']
             }
           ]
