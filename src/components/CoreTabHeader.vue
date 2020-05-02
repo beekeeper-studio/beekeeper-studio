4f4ed78a74c655d51bd8b24bf2173445dcd08153
Gregory <garden.greg@gmail.com>
2020-05-01 20:01:40 -0500

    tab pixel jump, codemirror syntax colors, misc

--- src/components/CoreTabHeader.vue
+++ src/components/CoreTabHeader.vue
@@ -8,7 +8,7 @@
       <i v-if="tab.type === 'table'" :class="iconClass" class="material-icons item-icon table">grid_on</i>
       <i v-if="tab.type === 'query'" class="material-icons item-icon query">code</i>
       <i v-if="tab.type === 'settings'" class="material-icons item-icon settings">settings</i>
-      <span class="expand truncate">{{title}}</span>
+      <span class="tab-title expand truncate" :title="title">{{title}}</span>
       <span class="tab-close" :class="{unsaved: tab.unsavedChanges}" @click.prevent.stop="$emit('close', tab)">
         <i class="material-icons close">close</i>
         <i class="material-icons unsaved-icon" >fiber_manual_record</i>
