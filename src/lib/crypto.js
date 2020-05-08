4713345aeeffb94ed40548e7b16d8f0b4de541d0
Danilo Polani <danilo.polani@weroad.it>
2020-05-08 16:45:40 +0200

    improve UUID generation adding Date.now as random factor

--- src/lib/crypto.js
+++ src/lib/crypto.js
@@ -27,8 +27,10 @@ export function decrypt(text, secret) {
 }
 
 export function uuidv4() {
-  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-    var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
-    return v.toString(16);
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, char => {
+    const random = (Date.now() + Math.random() * 16) % 16 | 0;
+    const value = char === 'x' ? random : (random & 0x3 | 0x8);
+
+    return value.toString(16);
   });
 }
