7ab2594481905fc0e18ddaae9a03c6deb4af1e0d
Danilo Polani <danilo.polani@weroad.it>
2020-05-12 13:25:38 +0200

    add ssl cert and key file to migration

--- src/migration/20200509.js
+++ src/migration/20200509.js
@@ -1,8 +1,12 @@
 
 export default {
-  name: "20200509-add-sslCaFile-column",
+  name: "20200509-add-sslFiles-columns",
   async run(runner) {
     await runner.query('ALTER TABLE saved_connection ADD COLUMN sslCaFile varchar')
+    await runner.query('ALTER TABLE saved_connection ADD COLUMN sslCertFile varchar')
+    await runner.query('ALTER TABLE saved_connection ADD COLUMN sslKeyFile varchar')
     await runner.query('ALTER TABLE used_connection ADD COLUMN sslCaFile varchar')
+    await runner.query('ALTER TABLE used_connection ADD COLUMN sslCertFile varchar')
+    await runner.query('ALTER TABLE used_connection ADD COLUMN sslKeyFile varchar')
   }
 }
