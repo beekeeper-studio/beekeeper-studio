40361d408de03b64dc7f9266f6b52019818eeefc
Matthew Rathbone <matthew.rathbone@gmail.com>
2020-05-22 21:46:24 -0500

    wrapped mysql identifiers

--- src/lib/db/clients/utils.js
+++ src/lib/db/clients/utils.js
@@ -60,7 +60,7 @@ export function buildSelectTopQuery(table, offset, limit, orderBy, filters) {
 
   if (filters && filters.length > 0) {
     filterString = "WHERE " + filters.map((item) => {
-      return `${item.field} ${item.type} ?`
+      return `\`${item.field}\` ${item.type} ?`
     }).join(" AND ")
 
     filterParams = filters.map((item) => {
@@ -69,7 +69,7 @@ export function buildSelectTopQuery(table, offset, limit, orderBy, filters) {
   }
 
   let baseSQL = `
-    FROM ${table}
+    FROM \`${table}\`
     ${filterString}
   `
   let countSQL = `
