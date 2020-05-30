7d34c36f8c8b8eac4235acba1e03a50296029a15
Matthew Peveler <matt.peveler@gmail.com>
2020-05-30 14:15:39 -0400

    fix for postgresql driver for redshift
    Amazon Redshift is based on postgresql 8.0, and pg_catalog.pg_type.typarray was added in 8.3, meaning that the query
    would cause an exception to be thrown due to el.typarray not existing when used for Redshift. This change gives us a
    fallback in the case of an exception on the first query to use one that will work under pg 8.0. Like the original query,
    this is based on the sql that is executed when using \dT command within psql, with the addition of the clause on
    n.snpname which was added in later versions of psql for \dT.

--- src/lib/db/clients/postgresql.js
+++ src/lib/db/clients/postgresql.js
@@ -29,16 +29,32 @@ pg.types.setTypeParser(1184, 'text', (val) => val); // timestamp
 
 
 async function getTypes(conn) {
-  const sql = `
-    SELECT      n.nspname as schema, t.typname as typename, t.oid::int4 as typeid
-    FROM        pg_type t
-    LEFT JOIN   pg_catalog.pg_namespace n ON n.oid = t.typnamespace
-    WHERE       (t.typrelid = 0 OR (SELECT c.relkind = 'c' FROM pg_catalog.pg_class c WHERE c.oid = t.typrelid))
-    AND     NOT EXISTS(SELECT 1 FROM pg_catalog.pg_type el WHERE el.oid = t.typelem AND el.typarray = t.oid)
-    AND     n.nspname NOT IN ('pg_catalog', 'information_schema');
-  `
+  let data;
+  try {
+    const sql = `
+      SELECT      n.nspname as schema, t.typname as typename, t.oid::int4 as typeid
+      FROM        pg_type t
+      LEFT JOIN   pg_catalog.pg_namespace n ON n.oid = t.typnamespace
+      WHERE       (t.typrelid = 0 OR (SELECT c.relkind = 'c' FROM pg_catalog.pg_class c WHERE c.oid = t.typrelid))
+      AND     NOT EXISTS(SELECT 1 FROM pg_catalog.pg_type el WHERE el.oid = t.typelem AND el.typarray = t.oid)
+      AND     n.nspname NOT IN ('pg_catalog', 'information_schema');
+    `
+
+    data = await driverExecuteQuery(conn, { query: sql })
+  } catch (err) {
+    const sql = `
+      SELECT      n.nspname as schema, t.typname as typename, t.oid::int4 as typeid
+      FROM        pg_type t
+      LEFT JOIN   pg_catalog.pg_namespace n ON n.oid = t.typnamespace
+      WHERE       (t.typrelid = 0 OR (SELECT c.relkind = 'c' FROM pg_catalog.pg_class c WHERE c.oid = t.typrelid))
+      AND     t.typname !~ '^_'
+      AND     n.nspname NOT IN ('pg_catalog', 'information_schema');
+    `
+
+    data = await driverExecuteQuery(conn, { query: sql })    
+  }
+  
   const result = {}
-  const data = await driverExecuteQuery(conn, { query: sql })
   data.rows.forEach((row) => {
     result[row.typeid] = row.typename
   })
