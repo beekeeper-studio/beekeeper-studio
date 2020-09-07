e67d31f4217b12b20ee67e3c1a8a0acba8354e43
Geovanni Pacheco <geovanni.m.pacheco@gmil.com>
2020-09-07 17:49:07 -0300

    Fix UsedConnection

--- src/common/appdb/models/used_connection.ts
+++ src/common/appdb/models/used_connection.ts
@@ -5,22 +5,21 @@ import { DbConnectionBase, SavedConnection } from './saved_connection'
 @Entity({ name: 'used_connection' })
 export class UsedConnection extends DbConnectionBase {
 
-  constructor(other: UsedConnection | SavedConnection) {
+  name?: string
+
+  constructor(other: SavedConnection) {
     super()
     if (other) {
       this.connectionType = other.connectionType
       this.defaultDatabase = other.defaultDatabase
       this.username = other.username
-      // TODO: Check case path is not implemented
-      // this.path = other.path
       this.uri = other.uri
       this.port = other.port
       this.host = other.host
       this.sshHost = other.sshHost
       this.sshPort = other.sshPort
       this.sshBastionHost = other.sshBastionHost
-      // TODO: Check case name is not implemented
-      // this.name = other.name
+      this.name = other.name
       if (other.id) {
         this.savedConnectionId = other.id
       }
