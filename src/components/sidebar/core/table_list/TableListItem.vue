68e8404de664a0bffdfe82414717c766ceb88c50
Matthew Rathbone <matthew.rathbone@gmail.com>
2020-09-17 12:07:45 -0500

    click on arrow for dropdown, double click for open table

--- src/components/sidebar/core/table_list/TableListItem.vue
+++ src/components/sidebar/core/table_list/TableListItem.vue
@@ -1,13 +1,14 @@
 <template>
   <div class="list-item">
-    <a class="list-item-btn" role="button" v-bind:class="{'active': selected,'open': showColumns }" @mousedown.prevent="toggleColumns">
-      <span class="btn-fab open-close" >
+    <a class="list-item-btn" role="button" v-bind:class="{'active': selected,'open': showColumns }">
+      <span class="btn-fab open-close" @mousedown.prevent="toggleColumns" >
         <i class="dropdown-icon material-icons">keyboard_arrow_right</i>
       </span>
-      <i :title="title" :class="iconClass" class="item-icon material-icons">grid_on</i>
-      <span class="table-name truncate expand">{{table.name}}</span>
+      <span class="item-wrapper" @mousedown.prevent="$emit('selected', table)" @dblclick.prevent="openTable">
+        <i :title="title" :class="iconClass" class="item-icon material-icons">grid_on</i>
+        <span class="table-name truncate expand">{{table.name}}</span>
+      </span>
       <span class="actions" v-bind:class="{'pinned': pinned.includes(table)}">
-        <span class="btn-fab launch" title="Open in a new tab" @mousedown.prevent.stop="openTable"><i class="material-icons">launch</i></span>
         <span v-if="!pinned.includes(table)" @mousedown.prevent.stop="pin" class="btn-fab pin" :title="'Pin'"><i class="bk-pin"></i></span>
         <span v-if="pinned.includes(table)" @mousedown.prevent.stop="unpin" class="btn-fab unpin" :title="'Unpin'"><i class="material-icons">clear</i></span>
         <span v-if="pinned.includes(table)" class="btn-fab pinned"><i class="bk-pin" :title="'Unpin'"></i></span>
