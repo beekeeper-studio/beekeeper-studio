05e845c38ea01d150f3afabb3f9ce619166a9df6
Gregory <garden.greg@gmail.com>
2020-10-12 17:52:21 -0500

    removing functions stuff from previous branch, no-tables styling cleanup

--- src/components/sidebar/core/TableList.vue
+++ src/components/sidebar/core/TableList.vue
@@ -52,7 +52,7 @@
     <!-- Tables -->
     <hr v-show="pinned.length > 0"> <!-- Fake splitjs Gutter styling -->
     <div v-if="!tablesLoading" class="table-list flex-col" ref="tables">
-      <nav class="list-group flex-col">
+      <nav class="list-group flex-col" v-show="tables.length > 0">
         <div class="list-heading row">
           <div class="sub row flex-middle expand">
             <!-- <span class="btn-fab open">
@@ -118,45 +118,6 @@
       {{tablesLoading}}
     </div>
 
-    <!-- Functions -->
-    <hr> <!-- Fake splitjs Gutter styling -->
-    <div class="table-list functions flex-col" ref="pinned">
-      <nav class="list-group flex-col">
-        <div class="list-heading row">
-          <div class="sub row flex-middle expand">
-            <!-- <span class="btn-fab open">
-              <i class="dropdown-icon material-icons">keyboard_arrow_down</i>
-            </span> -->
-            <div>Functions <span class="badge">{{pinned.length}}</span></div>
-          </div>
-        </div>
-        <div class="list-body">
-          <div class="list-item">
-            <a class="list-item-btn" role="button" >
-              <span class="btn-fab open-close" >
-                <i class="dropdown-icon material-icons">keyboard_arrow_right</i>
-              </span>
-              <span class="item-wrapper flex flex-middle expand">
-                <i :title="title" :class="iconClass" class="item-icon function-icon material-icons">functions</i>
-                <span class="table-name truncate">Function Name</span>
-              </span>
-              <span class="actions" >
-                <span class="btn-fab pin" :title="'Pin'"><i class="bk-pin"></i></span>
-                <span class="btn-fab unpin" :title="'Unpin'"><i class="material-icons">clear</i></span>
-                <!-- <span class="btn-fab pinned"><i class="bk-pin" :title="'Unpin'"></i></span> -->
-              </span>
-            </a>
-            <!-- <div  class="sub-items">
-              <span class="sub-item">
-                <span class="title truncate" ref="title" @click="selectColumn(i)">Sub Name</span>
-                <span class="badge" v-bind:class="c.dataType"><span>Type</span></span>
-              </span>
-            </div> -->
-
-          </div>
-        </div>
-      </nav>
-    </div>
 
   </div>
 </template>
