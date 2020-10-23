561a7628360e245589cb7b4ba3d8dbc0bec0e175
Matthew Rathbone <matthew.rathbone@gmail.com>
2020-10-23 17:21:32 -0500

    Update arm-publish.sh

--- build/arm-publish.sh
+++ build/arm-publish.sh
@@ -4,4 +4,3 @@ set -euxo pipefail
 yarn install --frozen-lockfile
 yarn run electron:build --arm64 --publish always
 yarn run electron:build --armv7l --publish always
-PI_BUILD=true yarn run electron:build --armv7l --publish always --linux deb snap
