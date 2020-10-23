1716f2396c3a2da269151e1576a6f5eb9e4d5933
Matthew Rathbone <matthew.rathbone@gmail.com>
2020-10-23 14:15:01 -0500

    don't publish armhf binaries. Let's update docs

--- build/arm-publish.sh
+++ build/arm-publish.sh
@@ -3,5 +3,4 @@
 set -euxo pipefail
 yarn install --frozen-lockfile
 yarn run electron:build --arm64 --publish always
-PI_BUILD=true yarn run electron:build --armv7l --publish always
-yarn run electron:build --armv7l --linux deb --publish always
+yarn run electron:build --armv7l --publish always
