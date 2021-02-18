1e341bbeb0c55e34d5e84a0b4698cfeadaedc690
Matthew Rathbone <matthew.rathbone@gmail.com>
2021-02-18 16:12:34 +0000

    incorrect fpm dependencies

--- vue.config.js
+++ vue.config.js
@@ -90,7 +90,7 @@ module.exports = {
           ],
           fpm: fpmOptions,
           // when we upgrade Electron we need to check these
-          depends: ["libgtk-3-0, libnotify4, libnss3, libxss1, libxtst6, xdg-utils, libatspi2.0-0, libuuid1, libappindicator3-1, libsecret-1-0", "gnupg"]
+          depends: ["libgtk-3-0", "libnotify4", "libnss3", "libxss1", "libxtst6", "xdg-utils", "libatspi2.0-0", "libuuid1", "libappindicator3-1", "libsecret-1-0", "gnupg"]
         },
         appImage: {
           publish: ['github']
