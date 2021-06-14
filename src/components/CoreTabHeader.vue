74011d14425c69c0bbb34c17e66e6d7112538262
No1Lik3U <SayedMustafaHussaini@outlook.de>
2021-06-14 21:47:00 +0200

    rename function of the favorite queries

--- src/components/CoreTabHeader.vue
+++ src/components/CoreTabHeader.vue
@@ -7,15 +7,40 @@
         @click.middle.prevent="$emit('close', tab)"
         :class="{ active: selected }"
       >
-        <i v-if="tab.type === 'table'" :class="iconClass" class="material-icons item-icon table">grid_on</i>
-        <i v-if="tab.type === 'query'" class="material-icons item-icon query">code</i>
-        <i v-if="tab.type === 'table-properties'" class="material-icons item-icon table-properties" :class="iconClass">lightbulb</i>
-        <i v-if="tab.type === 'settings'" class="material-icons item-icon settings">settings</i>
-        <span class="tab-title truncate" :title="title + scope">{{title}} <span v-if="scope" class="tab-title-scope">{{scope}}</span></span>
+        <i
+          v-if="tab.type === 'table'"
+          :class="iconClass"
+          class="material-icons item-icon table"
+          >grid_on</i
+        >
+        <i v-if="tab.type === 'query'" class="material-icons item-icon query"
+          >code</i
+        >
+        <i
+          v-if="tab.type === 'table-properties'"
+          class="material-icons item-icon table-properties"
+          :class="iconClass"
+          >lightbulb</i
+        >
+        <i
+          v-if="tab.type === 'settings'"
+          class="material-icons item-icon settings"
+          >settings</i
+        >
+        <span class="tab-title truncate" :title="title + scope" ref="tabTitle">
+          <span> {{ title }}</span>
+          <span v-if="scope" class="tab-title-scope">{{ scope }}</span>
+        </span>
+
         <div class="tab-action">
-          <span class="tab-close" :class="{unsaved: tab.unsavedChanges}" @mousedown.stop="doNothing" @click.prevent.stop="$emit('close', tab)">
+          <span
+            class="tab-close"
+            :class="{ unsaved: tab.unsavedChanges }"
+            @mousedown.stop="doNothing"
+            @click.prevent.stop="$emit('close', tab)"
+          >
             <i class="material-icons close">close</i>
-            <i class="material-icons unsaved-icon" >fiber_manual_record</i>
+            <i class="material-icons unsaved-icon">fiber_manual_record</i>
           </span>
         </div>
       </a>
@@ -32,7 +57,7 @@
         <x-menuitem @click.prevent="$emit('closeAll')">
           <x-label>Close All</x-label>
         </x-menuitem>
-        <hr>
+        <hr />
         <x-menuitem @click.prevent="$emit('duplicate', tab)">
           <x-label>Duplicate</x-label>
         </x-menuitem>
@@ -41,71 +66,66 @@
   </div>
 </template>
 <script>
-
-  export default {
-    props: ['tab', 'tabsCount', 'selected'],
-    data() {
-      return {
-        unsaved: false,
+export default {
+  props: ["tab", "tabsCount", "selected"],
+  data() {
+    return {
+      unsaved: false
+    };
+  },
+  methods: {
+    doNothing() {},
+    mousedown(e) {
+      if (e.which === 1) {
+        this.$emit("click", this.tab);
+      }
+    }
+  },
+  watch: {},
+  computed: {
+    cleanText() {
+      // no spaces
+      if (!this.tab.text) {
+        return null;
+      }
+      const result = this.tab.text.replace(/\s+/, "");
+      return result.length == 0 ? null : result;
+    },
+    iconClass() {
+      const result = {};
+      result[`${this.tab.table.entityType}-icon`] = true;
+      return result;
+    },
+    scope() {
+      if (this.tab.titleScope) {
+        return " " + "[" + this.tab.titleScope + "]";
+      } else {
+        return "";
       }
     },
-    methods: {
-      doNothing() {
-        
-      },
-      mousedown(e) {
-        if (e.which === 1) {
-          this.$emit('click', this.tab)
-        }
+    tableTabTitle() {
+      if (!this.tab.type === "table") return null;
+      let result = this.tab.table.name;
+      return result;
+    },
+    queryTabTitle() {
+      if (!this.tab.type === "query") return null;
+      if (this.tab.query && this.tab.query.title) {
+        return this.tab.query.title;
+      }
+      if (!this.cleanText) {
+        return this.tab.title;
+      }
+
+      if (this.tab.query.text.length >= 32) {
+        return `${this.tab.query.text.substring(0, 32)}...`;
+      } else {
+        return this.tab.query.text;
       }
     },
-    watch: {
-    },
-    computed: {
-      cleanText() {
-        // no spaces
-        if (!this.tab.text) {
-          return null
-        }
-        const result = this.tab.text.replace(/\s+/, '')
-        return result.length == 0 ? null : result
-      },
-      iconClass() {
-        const result = {}
-        result[`${this.tab.table.entityType}-icon`] = true
-        return result
-      },
-      scope() {
-        if (this.tab.titleScope) {
-          return ' ' + '[' + this.tab.titleScope + ']'
-        } else {
-          return ''
-        }
-      },
-      tableTabTitle() {
-        if (!this.tab.type === 'table') return null;
-        let result = this.tab.table.name
-        return result
-      },
-      queryTabTitle() {
-        if (!this.tab.type === 'query') return null
-          if (this.tab.query && this.tab.query.title) {
-            return this.tab.query.title
-          }
-          if (!this.cleanText) {
-            return this.tab.title
-          }
-
-          if (this.tab.query.text.length >= 32) {
-            return `${this.tab.query.text.substring(0, 32)}...`
-          } else {
-            return this.tab.query.text
-          }
-      },
-      title() {
-        return this.queryTabTitle || this.tableTabTitle || "Unknown"
-      },
+    title() {
+      return this.queryTabTitle || this.tableTabTitle || "Unknown";
     }
   }
-
+};
 </script>
--- src/components/sidebar/core/FavoriteList.vue
+++ src/components/sidebar/core/FavoriteList.vue
@@ -2,17 +2,40 @@
   <div class="sidebar-favorites flex-col expand">
     <div class="sidebar-list">
       <nav class="list-group" v-if="favorites.length > 0">
-        <div class="list-item" v-for="item in favorites" v-bind:key="item.id">
-          <a class="list-item-btn" @click.prevent="click(item)" :class="{active: selected(item)}">
+        <div class="list-item" v-for="item in favorites" :key="item.id">
+          <a
+            class="list-item-btn"
+            @click.prevent="click(item)"
+            :class="{ active: selected(item) }"
+          >
             <i class="item-icon query material-icons">code</i>
             <!-- <input @click.stop="" type="checkbox" :value="item" class="form-control delete-checkbox" v-model="checkedFavorites" :class="{ shown: checkedFavorites.length > 0 }"> -->
             <div class="list-title flex-col">
-              <span class="item-text title truncate expand" :title="item.title">{{item.title}}</span>
-              <span class="database subtitle"><span :title="item.database" >{{item.database}}</span></span>
+              <span
+                class="item-text title truncate expand"
+                :ref="`title-${item.id}`"
+                :title="item.title"
+                >{{ item.title }}</span
+              >
+              <input
+                type="text"
+                :ref="`input-${item.id}`"
+                v-model="renameSystem.title"
+                @keydown.enter="rename(item)"
+                style="display:none;"
+                autofocus
+              />
+
+              <span class="database subtitle">
+                <span :title="item.database">{{ item.database }} </span>
+              </span>
             </div>
             <x-contextmenu>
               <x-menu style="--target-align: right; --v-target-align: top;">
-                <x-menuitem @click="remove(item)">
+                <x-menuitem>
+                  <x-label @click="triggerRename(item)">Rename</x-label>
+                </x-menuitem>
+                <x-menuitem @click.prevent="remove(item)">
                   <x-label class="text-danger">Remove</x-label>
                 </x-menuitem>
               </x-menu>
@@ -24,6 +47,19 @@
         <span>No Saved Queries</span>
       </div>
     </div>
+    <x-contextmenu>
+      <x-menu style="--target-align: right; --v-target-align: top;">
+        <x-menuitem>
+          <x-label @click="createFolder">New Folder</x-label>
+          <x-icon
+            name="folder"
+            computedsize="small"
+            size="small"
+            iconset="https://xel-toolkit.org/iconsets/material.svg"
+          ></x-icon>
+        </x-menuitem>
+      </x-menu>
+    </x-contextmenu>
     <!-- <div class="toolbar btn-group row flex-right" v-show="checkedFavorites.length > 0">
       <a class="btn btn-link" @click="discardCheckedFavorites">Cancel</a>
       <a class="btn btn-primary" :title="removeTitle" @click="removeCheckedFavorites">Remove</a>
@@ -32,39 +68,83 @@
 </template>
 
 <script>
-  import { mapState } from 'vuex'
-  export default {
-    data: function () {
-      return {
-        checkedFavorites: []
+import { mapState } from "vuex";
+import { createParentDirectory } from "common/utils";
+export default {
+  data() {
+    return {
+      checkedFavorites: [],
+      renameSystem: {
+        input: null,
+        span: null,
+        title: null,
+        itemId: null
+      }
+    };
+  },
+  computed: {
+    ...mapState(["favorites", "activeTab"]),
+    removeTitle() {
+      return `Remove ${this.checkedFavorites.length} saved queries`;
+    }
+  },
+  methods: {
+    // other trigger needed because files are multiplied not components
+    triggerRename(item) {
+      const input = this.$refs[`input-${item.id}`][0];
+      const span = this.$refs[`title-${item.id}`][0];
+      input.style.display = "block";
+      span.style.display = "none";
+      if (
+        this.renameSystem.itemId !== null &&
+        item.id !== this.renameSystem.itemId
+      ) {
+        this.resetLastRenameInput(item);
+      }
+      this.renameSystem.itemId = item.id;
+      this.renameSystem.title = item.title;
+    },
+
+    resetLastRenameInput(item) {
+      const lastInput = this.$refs[`input-${this.renameSystem.itemId}`][0];
+      const lastSpan = this.$refs[`title-${this.renameSystem.itemId}`][0];
+      lastInput.style.display = "none";
+      lastSpan.style.display = "block";
+    },
+
+    async rename(item) {
+      if (this.renameSystem.title !== "") {
+        item.title = this.renameSystem.title;
+        await this.$store.dispatch("renameFavorite", item);
+        this.resetLastRenameInput(item);
       }
     },
-    computed: {
-      ...mapState(['favorites', 'activeTab']),
-      removeTitle() {
-        return `Remove ${this.checkedFavorites.length} saved queries`;
-      }
+
+    createFolder() {},
+
+    selected(item) {
+      return (
+        this.activeTab &&
+        this.activeTab.query &&
+        this.activeTab.query.id === item.id
+      );
     },
-    methods: {
-      selected(item) {
-        return this.activeTab && this.activeTab.query &&
-          this.activeTab.query.id === item.id
-      },
-      click(item) {
-        this.$root.$emit('favoriteClick', item)
-      },
-      async remove(favorite) {
-        await this.$store.dispatch('removeFavorite', favorite)
-      },
-      async removeCheckedFavorites() {
-        for(let i = 0; i < this.checkedFavorites.length; i++) {
-          await this.remove(this.checkedFavorites[i])
-        }
-        this.checkedFavorites = [];
-      },
-      discardCheckedFavorites() {
-        this.checkedFavorites = [];
+    click(item) {
+      this.$root.$emit("favoriteClick", item);
+    },
+    async remove(favorite) {
+      console.log(favorite.remove());
+      await this.$store.dispatch("removeFavorite", favorite);
+    },
+    async removeCheckedFavorites() {
+      for (let i = 0; i < this.checkedFavorites.length; i++) {
+        await this.remove(this.checkedFavorites[i]);
       }
+      this.checkedFavorites = [];
+    },
+    discardCheckedFavorites() {
+      this.checkedFavorites = [];
     }
   }
+};
 </script>
