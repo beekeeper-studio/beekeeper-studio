dfcd2b1b98d90f22b5d3ceba145ee4b9f842a1b8
No1Lik3U <SayedMustafaHussaini@outlook.de>
2021-06-15 23:12:18 +0200

    builtree function

--- src/components/sidebar/CoreSidebar.vue
+++ src/components/sidebar/CoreSidebar.vue
@@ -1,5 +1,4 @@
 <template>
-
   <div class="sidebar-wrap row">
     <global-sidebar
       @selected="click"
@@ -8,8 +7,6 @@
     ></global-sidebar>
 
     <div class="tab-content">
-
-
       <!-- Tables -->
       <div
         class="tab-pane"
@@ -17,7 +14,10 @@
         :class="tabClasses('tables')"
         v-show="activeItem === 'tables'"
       >
-        <database-dropdown @databaseSelected="databaseSelected" :connection="connection"></database-dropdown>
+        <database-dropdown
+          @databaseSelected="databaseSelected"
+          :connection="connection"
+        ></database-dropdown>
         <table-list></table-list>
       </div>
 
@@ -46,74 +46,80 @@
           <span class="sidebar-title">Saved Queries</span>
           <span class="expand"></span>
         </div>
-        <favorite-list></favorite-list>
+        <favorite-folder></favorite-folder>
       </div>
-
     </div>
-
   </div>
 </template>
 
 <script>
-  import _ from 'lodash'
-  import GlobalSidebar from './GlobalSidebar'
-  import TableList from './core/TableList'
-  import HistoryList from './core/HistoryList'
-  import FavoriteList from './core/FavoriteList'
-  import DatabaseDropdown from './core/DatabaseDropdown'
+import _ from "lodash";
+import GlobalSidebar from "./GlobalSidebar";
+import TableList from "./core/TableList";
+import HistoryList from "./core/HistoryList";
+import FavoriteFolder from "./core/favorite_list/FavoriteFolder";
+// import FavoriteList from './core/FavoriteList'
+import DatabaseDropdown from "./core/DatabaseDropdown";
 
-  import { mapState } from 'vuex'
+import { mapState } from "vuex";
+import FavoriteFolder from "./core/favorite_list/FavoriteFolder.vue";
 
-  export default {
-    props: ['sidebarShown'],
-    components: { TableList, DatabaseDropdown, HistoryList, GlobalSidebar, FavoriteList},
-    data() {
+export default {
+  props: ["sidebarShown"],
+  components: {
+    TableList,
+    DatabaseDropdown,
+    HistoryList,
+    GlobalSidebar,
+    FavoriteFolder
+  },
+  data() {
+    return {
+      tableLoadError: null,
+      selectedTable: null,
+      filterQuery: null,
+      allExpanded: null,
+      allCollapsed: null,
+      activeItem: "tables"
+    };
+  },
+  computed: {
+    filteredTables() {
+      if (!this.filterQuery) {
+        return this.tables;
+      }
+      const startsWithFilter = _(this.tables)
+        .filter(item => _.startsWith(item.name, this.filterQuery))
+        .value();
+      const containsFilter = _(this.tables)
+        .difference(startsWithFilter)
+        .filter(item => item.name.includes(this.filterQuery))
+        .value();
+      return _.concat(startsWithFilter, containsFilter);
+    },
+    ...mapState(["tables", "connection", "database"])
+  },
+  methods: {
+    tabClasses(item) {
       return {
-        tableLoadError: null,
-        selectedTable: null,
-        filterQuery: null,
-        allExpanded: null,
-        allCollapsed: null,
-        activeItem: 'tables',
+        show: this.activeItem === item,
+        active: this.activeItem === item
+      };
+    },
+    click(item) {
+      this.activeItem = item;
+      if (!this.sidebarShown) {
+        this.$emit("toggleSidebar");
       }
     },
-    computed: {
-      filteredTables() {
-        if (!this.filterQuery) {
-          return this.tables
-        }
-        const startsWithFilter = _(this.tables)
-          .filter((item) => _.startsWith(item.name, this.filterQuery))
-          .value()
-        const containsFilter = _(this.tables)
-          .difference(startsWithFilter)
-          .filter((item) => item.name.includes(this.filterQuery))
-          .value()
-        return _.concat(startsWithFilter, containsFilter)
-      },
-      ...mapState(['tables', 'connection', 'database']),
+    async databaseSelected(db) {
+      await this.$store.dispatch("changeDatabase", db);
+      this.allExpanded = false;
     },
-    methods: {
-      tabClasses(item) {
-        return {
-          show: (this.activeItem === item),
-          active: (this.activeItem === item)
-        }
-      },
-      click(item) {
-        this.activeItem = item;
-        if(!this.sidebarShown) {
-          this.$emit('toggleSidebar')
-        }
-      },
-      async databaseSelected(db) {
-        await this.$store.dispatch('changeDatabase', db)
-        this.allExpanded = false
-      },
-      async disconnect() {
-        await this.$store.dispatch('disconnect')
-        this.$noty.success("Successfully Disconnected")
-      },
+    async disconnect() {
+      await this.$store.dispatch("disconnect");
+      this.$noty.success("Successfully Disconnected");
     }
   }
+};
 </script>
---
+++ src/plugins/foldertree.js
@@ -0,0 +1,40 @@
+const fs = require("fs");
+
+const options = {};
+
+class TreeNode {
+  constructor(path) {
+    this.path = path;
+    this.children = [];
+  }
+}
+
+export function buildTree(rootPath) {
+  const root = new TreeNode(rootPath);
+  const stack = [root];
+
+  while (stack.length) {
+    const currentNode = stack.pop();
+
+    if (currentNode) {
+      const children = fs.readdirSync(currentNode.path);
+
+      children.forEach((file) => {
+        const childPath = `${currentNode.path}/${file}`;
+        const childNode = new TreeNode(childPath);
+
+        currentNode.children.push(childNode);
+
+        if (fs.statSync(childNode.path).isDirectory()) {
+          stack.push(childNode);
+        }
+      });
+    }
+  }
+
+  return root;
+}
+
+function updateTree(path) {}
+
+function deleteTreeNode(path) {}
