f1b948b28be041cc8db0aa8c6a333f85430ca969
Matthew Rathbone <matthew.rathbone@gmail.com>
2021-06-15 08:51:28 -0500

    tsconfig and lint cleanup

--- packages/components/.eslintrc.js
+++ packages/components/.eslintrc.js
@@ -15,6 +15,7 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
   },
+  ignorePatterns: ['dist', 'node_modules'],
   overrides: [
     {
       files: [
