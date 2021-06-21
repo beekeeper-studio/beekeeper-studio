288c4d839d33c5aa00812e2dd91102e362866446
ghstng <garden.greg@gmail.com>
2021-06-21 15:31:39 -0500

    init templates layout

--- apps/sqltools/src/views/Templates.vue
+++ apps/sqltools/src/views/Templates.vue
@@ -3,15 +3,19 @@
     <section class="subheader">
       <div class="big-wrap">
         <h1>This is an templates page</h1>
-        <div v-for="template in templates" :key="template.name">
-          <h2>{{template.name}}</h2>
-          <p>{{template.description}}</p>
-        </div>
       </div>
     </section>
     <section>
       <div class="big-wrap">
-        <router-link :to="{ name: 'Template', params: {name: template.name}}">View</router-link>
+        <div class="row gutter">
+            <div v-for="template in templates" :key="template.name" class="col s6">
+              <router-link :to="{ name: 'Template', params: {name: template.name}}" class="card-flat">
+                <h2>{{template.name}}</h2>
+                <p>{{template.description}}</p>
+                <span class="text-primary">View</span>
+              </router-link>
+            </div>
+        </div>
       </div>
     </section>
   </div>
@@ -34,3 +38,25 @@ export default Vue.extend({
   }
 })
 </script>
+
+<style lang="scss" scoped>
+  @import '@/assets/styles/app/_variables';
+  
+  section {
+    padding: $gutter-w * 2;
+  }
+  .subheader {
+    background: $query-editor-bg;
+  }
+  .card-flat {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    justify-content: flex-start;
+    padding: $gutter-w * 1.5;
+    flex-grow: 1;
+    h2 {
+      margin: 0;
+    }
+  }
+</style>
