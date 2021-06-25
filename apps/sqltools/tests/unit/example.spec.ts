b5ed535dc277c0fa661eed66ddd3f8b39d44a6b0
Matthew Rathbone <matthew.rathbone@gmail.com>
2021-06-25 09:10:29 -0500

    running tests

--- apps/sqltools/tests/unit/example.spec.ts
+++ apps/sqltools/tests/unit/example.spec.ts
@@ -1,12 +1,6 @@
-import { shallowMount } from '@vue/test-utils'
-import HelloWorld from '@/components/HelloWorld.vue'
 
-describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg }
-    })
-    expect(wrapper.text()).toMatch(msg)
+describe('Canary Test', () => {
+  it('asserts true', () => {
+    expect(true).toBe(true)
   })
 })
