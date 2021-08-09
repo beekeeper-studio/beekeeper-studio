e377bee9c3e271ddaa465a052174bafd74b11b6a
Matthew Rathbone <matthew.rathbone@gmail.com>
2021-08-09 16:39:53 -0500

    exports were missing 2 'await' keywords, also json fixes for sql

---
+++ apps/studio/tests/integration/lib/export/formats/csv.spec.js
@@ -0,0 +1,45 @@
+import { DBTestUtil } from "../../../../lib/db";
+import { CsvExporter } from '../../../../../src/lib/export/formats/csv'
+import tmp from 'tmp'
+import fs from 'fs'
+
+describe("CSV Exporter", () => {
+  let dbfile;
+  let util
+
+  beforeAll(async () => {
+    dbfile = tmp.fileSync()
+
+    const config = {
+      client: 'sqlite',
+    }
+    util = new DBTestUtil(config, dbfile.name, { dialect: 'sqlite' })
+    util.extraTables = 1
+    await util.setupdb()
+    // addresses has one entry
+    await util.knex.schema.createTable("toexport", (t)=> {
+      t.string("name")
+    })
+    await util.knex("toexport").insert([
+      { name: 'foo'},
+      { name: 'bar'},
+      { name: 'ba\nz'}
+    ]);
+
+  })
+
+  it("should create a simple csv export", async () => {
+    const filename = tmp.tmpNameSync()
+    const exporter = new CsvExporter(
+      filename,
+      util.connection,
+      { name: 'toexport'},
+      [],
+      { deleteOnAbort: true, chunkSize: 1},
+      { header: true, delimiter: ','}
+    )
+    await exporter.exportToFile()
+    const result = fs.readFileSync(filename, { encoding: 'utf-8'})
+    expect(result).toEqual('name\nfoo\nbar\n"ba\nz"\n')
+  })
+})
