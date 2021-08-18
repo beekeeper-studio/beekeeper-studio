7b3484acbb961a2999a9291c00bad907b666829e
Sayed Mustafa Hussaini <SayedMustafaHussaini@outlook.de>
2021-08-19 00:50:59 +0200

    first changes

--- src/components/sidebar/core/WorkspaceList.vue
+++ src/components/sidebar/core/WorkspaceList.vue
@@ -2,44 +2,21 @@
   <div :class="`sidebar-workspace flex-col expand ${isEmpty}`">
     <div class="sidebar-list" v-if="workspaces.length > 0">
       <nav class="list-group">
-        <div
-          class="list-item"
+        <workspace-list-item
           v-for="workspace in workspaces"
           :key="workspace.id"
-        >
-          <a class="list-item-btn" @click="select(workspace)">
-            <i class="item-icon material-icons">widgets</i>
-
-            <div class="list-title flex-col">
-              <span class="item-text expand truncate">
-                {{ nicelySized(workspace.title) }}</span
-              >
-              <span class="subtitle"
-                ><span>{{ workspace.createdAt }}</span></span
-              >
-            </div>
-            <x-contextmenu v-if="!noElement">
-              <x-menu style="--target-align: right; --v-target-align: top;">
-                <x-menuitem @click="remove(workspace)">
-                  <x-label class="text-danger">Remove</x-label>
-                </x-menuitem>
-              </x-menu>
-            </x-contextmenu>
-          </a>
-        </div>
+          :workspace="workspace"
+        ></workspace-list-item>
       </nav>
-      <x-contextmenu v-if="noElement">
-        <x-menu>
-          <x-menuitem @click.prevent="renameState">
-            <x-label>Rename</x-label>
-          </x-menuitem>
-          <hr />
-          <x-menuitem @click.prevent="remove(query)">
-            <x-label class="text-danger">Remove</x-label>
-          </x-menuitem>
-        </x-menu>
-      </x-contextmenu>
     </div>
+
+    <x-contextmenu>
+      <x-menu>
+        <x-menuitem>
+          <x-label>Create Workspace</x-label>
+        </x-menuitem>
+      </x-menu>
+    </x-contextmenu>
 
     <div class="empty" v-if="workspaces.length === 0">
       <span>No Workspaces found</span>
@@ -49,51 +26,14 @@
         <i class="item-icon material-icons left-margin">widgets</i></span
       >
     </div>
-
-    <!-- workspace-create-modal -->
-    <modal
-      class="vue-dialog beekeeper-modal shorter-width"
-      name="workspace-create-modal"
-      @opened="selectTitleInput"
-      height="auto"
-      :scrollable="true"
-    >
-      <form @submit.prevent="create" v-hotkey="keymap">
-        <div class="dialog-content">
-          <div class="dialog-c-title">Workspace name</div>
-          <div class="modal-form">
-            <div class="alert alert-danger save-errors" v-if="error">
-              {{ saveError }}
-            </div>
-            <div class="form-group">
-              <input
-                type="text"
-                ref="titleInput"
-                name="title"
-                class="form-control"
-                placeholder="Your workpsace name"
-                v-model="title"
-                autofocus
-              />
-            </div>
-          </div>
-        </div>
-        <div class="vue-dialog-buttons">
-          <button class="btn btn-flat" type="button" @click.prevent="cancel">
-            Cancel
-          </button>
-          <button class="btn btn-primary" type="submit">
-            Create
-          </button>
-        </div>
-      </form>
-    </modal>
   </div>
 </template>
 
 <script>
 import { Directory } from "@/common/appdb/models/directory";
+import WorkspaceListItem from "./explorer_list/WorkspaceListItem.vue";
 export default {
+  components: { WorkspaceListItem },
   data() {
     return {
       title: "",
@@ -124,18 +64,6 @@ export default {
   },
 
   methods: {
-    nicelySized(text) {
-      if (text.length >= 128) {
-        return `${text.substring(0, 128)}...`;
-      } else {
-        return text;
-      }
-    },
-
-    select(workspace) {
-      this.$emit("select", workspace);
-    },
-
     async create() {
       const reg = new RegExp(this.validation.dir);
 
--- src/components/sidebar/core/explorer_list/WorkspaceListItem.vue
+++ src/components/sidebar/core/explorer_list/WorkspaceListItem.vue
@@ -1,13 +1,75 @@
 <template>
-  
+  <div class="list-item">
+    <a class="list-item-btn" @click="select(workspace)">
+      <i class="item-icon material-icons">widgets</i>
+
+      <div class="list-title flex-col">
+        <span class="item-text expand truncate">
+          {{ nicelySized(workspace.title) }}</span
+        >
+        <span class="subtitle"
+          ><span>{{ workspace.createdAt }}</span></span
+        >
+      </div>
+      <x-contextmenu>
+        <x-menu style="--target-align: right; --v-target-align: top;">
+          <x-menuitem @click="remove(workspace)">
+            <x-label class="text-danger">Remove</x-label>
+          </x-menuitem>
+        </x-menu>
+      </x-contextmenu>
+    </a>
+  </div>
 </template>
 
 <script>
 export default {
+  props: ["workspace"],
+  data() {
+    return {};
+  },
 
-}
+  computed: {},
+
+  methods: {
+    nicelySized(text) {
+      if (text.length >= 128) {
+        return `${text.substring(0, 128)}...`;
+      } else {
+        return text;
+      }
+    },
+
+    async remove(workspace) {
+      await this.$store.dispatch("removeWorkspace", workspace);
+    },
+
+    select(workspace) {
+      this.$root.$emit("selectWorkspace", workspace);
+    }
+  }
+};
 </script>
 
-<style>
+<style lang="scss" scoped>
+.center-empty {
+  display: grid;
+  justify-content: center;
+  align-items: center;
+}
 
+.align {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+}
+
+.left-margin {
+  margin-left: 5px;
+}
+
+.shoter-widht {
+  width: 300px !important;
+}
 </style>
