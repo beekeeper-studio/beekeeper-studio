eb1bd94e5173c7567fc6f664461ae1368f19c341
Matthew Rathbone <matthew.rathbone@gmail.com>
2021-09-20 14:13:21 -0500

    test all the way down to 9.4

--- apps/studio/tests/integration/lib/db/clients/postgres.spec.ts
+++ apps/studio/tests/integration/lib/db/clients/postgres.spec.ts
@@ -4,7 +4,7 @@ import { Duration, TemporalUnit } from "node-duration"
 import { runCommonTests } from './all'
 import { IDbConnectionServerConfig } from '@/lib/db/client'
 
-const TEST_VERSIONS = ['9.6', 'latest']
+const TEST_VERSIONS = ['9.4', 'latest']
 
 function testWith(dockerTag) {
   describe(`Postgres [${dockerTag}]`, () => {
