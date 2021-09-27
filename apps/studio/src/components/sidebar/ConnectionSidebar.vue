ab5f220d76483a03e0baa814b9b9bc4ffa6a6699
ghstng <garden.greg@gmail.com>
2021-09-27 14:05:01 -0500

    init layout

--- apps/studio/src/components/sidebar/ConnectionSidebar.vue
+++ apps/studio/src/components/sidebar/ConnectionSidebar.vue
@@ -1,87 +1,90 @@
 <template>
-  <div class="sidebar-wrap flex-col">
+  <div class="sidebar-wrap flex">
+    <workspace-sidebar></workspace-sidebar>
 
     <!-- QUICK CONNECT -->
-    <div class="btn-wrap quick-connect">
-      <a
-        href=""
-        class="btn btn-flat btn-icon btn-block"
-        :class="{'active': defaultConfig == selectedConfig }"
-        @click.prevent="edit(defaultConfig)"
-      >
-      <i class="material-icons">offline_bolt</i>
-      <span>Quick Connect</span>
-      </a>
-    </div>
-
-    <div class="connection-wrap expand flex-col">
-
-      <!-- Saved Connections -->
-      <div class="list saved-connection-list expand" ref="savedConnectionList">
-        <div class="list-group">
-          <div class="list-heading">
-            <div class="flex flex-between">
-              <div class="sub row flex-middle noselect">
-                Saved Connections <span class="badge">{{connectionConfigs.length}}</span>
-              </div>
-              <x-button class="actions-btn btn btn-link" title="Sort By">
-                <span>{{sortables[this.sortOrder]}}</span>
-                <i class="material-icons">arrow_drop_down</i>
-                <x-menu>
-                  <x-menuitem
-                    v-for="i in Object.keys(sortables)"
-                    :key="i"
-                    :toggled="i === sortOrder"
-                    togglable
-                    @click="sortConnections(i)"
-                  >
-                    <x-label>{{ sortables[i] }}</x-label>
-                  </x-menuitem>
-                </x-menu>
-              </x-button>
-            </div>
-          </div>
-          <nav class="list-body">
-              <connection-list-item
-                v-for="c in orderedConnectionConfigs"
-                :key="c.id"
-                :config="c"
-                :selectedConfig="selectedConfig"
-                :showDuplicate="true"
-                @edit="edit"
-                @remove="remove"
-                @duplicate="duplicate"
-                @doubleClick="connect"
-              >
-              </connection-list-item>
-          </nav>
-        </div>
+    <div class="flex-col expand">
+      <div class="btn-wrap quick-connect">
+        <a
+          href=""
+          class="btn btn-flat btn-icon btn-block"
+          :class="{'active': defaultConfig == selectedConfig }"
+          @click.prevent="edit(defaultConfig)"
+        >
+        <i class="material-icons">offline_bolt</i>
+        <span>Quick Connect</span>
+        </a>
       </div>
-
-      <hr> <!-- Fake gutter for split.js -->
-
-      <!-- Recent Connections -->
-      <div class="list recent-connection-list expand" ref="recentConnectionList">
-        <div class="list-group">
-          <div class="list-heading">
-            <div class="sub row flex-middle noselect">
-              Recent Connections <span class="badge">{{usedConfigs.length}}</span>
+  
+      <div class="connection-wrap expand flex-col">
+  
+        <!-- Saved Connections -->
+        <div class="list saved-connection-list expand" ref="savedConnectionList">
+          <div class="list-group">
+            <div class="list-heading">
+              <div class="flex flex-between">
+                <div class="sub row flex-middle noselect">
+                  Saved Connections <span class="badge">{{connectionConfigs.length}}</span>
+                </div>
+                <x-button class="actions-btn btn btn-link" title="Sort By">
+                  <span>{{sortables[this.sortOrder]}}</span>
+                  <i class="material-icons">arrow_drop_down</i>
+                  <x-menu>
+                    <x-menuitem
+                      v-for="i in Object.keys(sortables)"
+                      :key="i"
+                      :toggled="i === sortOrder"
+                      togglable
+                      @click="sortConnections(i)"
+                    >
+                      <x-label>{{ sortables[i] }}</x-label>
+                    </x-menuitem>
+                  </x-menu>
+                </x-button>
+              </div>
             </div>
+            <nav class="list-body">
+                <connection-list-item
+                  v-for="c in orderedConnectionConfigs"
+                  :key="c.id"
+                  :config="c"
+                  :selectedConfig="selectedConfig"
+                  :showDuplicate="true"
+                  @edit="edit"
+                  @remove="remove"
+                  @duplicate="duplicate"
+                  @doubleClick="connect"
+                >
+                </connection-list-item>
+            </nav>
           </div>
-          <nav class="list-body">
-              <connection-list-item
-                v-for="c in usedConfigs"
-                :key="c.id"
-                :config="c"
-                :selectedConfig="selectedConfig"
-                :isRecentList="true"
-                :showDuplicate="false"
-                @edit="edit"
-                @remove="removeUsedConfig"
-                @doubleClick="connect"
-              >
-              </connection-list-item>
-          </nav>
+        </div>
+  
+        <hr> <!-- Fake gutter for split.js -->
+  
+        <!-- Recent Connections -->
+        <div class="list recent-connection-list expand" ref="recentConnectionList">
+          <div class="list-group">
+            <div class="list-heading">
+              <div class="sub row flex-middle noselect">
+                Recent Connections <span class="badge">{{usedConfigs.length}}</span>
+              </div>
+            </div>
+            <nav class="list-body">
+                <connection-list-item
+                  v-for="c in usedConfigs"
+                  :key="c.id"
+                  :config="c"
+                  :selectedConfig="selectedConfig"
+                  :isRecentList="true"
+                  :showDuplicate="false"
+                  @edit="edit"
+                  @remove="removeUsedConfig"
+                  @doubleClick="connect"
+                >
+                </connection-list-item>
+            </nav>
+          </div>
         </div>
       </div>
     </div>
@@ -90,11 +93,12 @@
 
 <script>
   import _ from 'lodash'
+  import WorkspaceSidebar from './WorkspaceSidebar'
   import { mapState, mapGetters } from 'vuex'
   import ConnectionListItem from './connection/ConnectionListItem'
   import Split from 'split.js'
   export default {
-    components: { ConnectionListItem },
+    components: { ConnectionListItem, WorkspaceSidebar },
     props: ['defaultConfig', 'selectedConfig'],
     data: () => ({
       split: null,
---
+++ apps/studio/src/components/sidebar/WorkspaceSidebar.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="workspace global-items">
+      <a
+        class="nav-item active"
+        title="Personal Workspace"
+      >
+        <span class="avatar"><i class="material-icons">person</i></span>
+      </a>
+      <div class="divider"><hr></div>
+      <a
+        class="nav-item"
+        title="Workspace"
+      >
+        <span class="avatar">Te</span>
+      </a>
+      <a
+        class="nav-item"
+        title="Add Workspace"
+      >
+        <span class="material-icons">add</span>
+      </a>
+    </div>
+</template>
+
+<script>
+
+  export default {
+    props: ['activeItem'],
+    computed: {
+    },
+    methods: {
+      // click(item) {
+      //   if( this.activeItem === item ){
+      //     this.$emit('toggleSidebar')
+      //   } else {
+      //     this.$emit('selected', item)
+      //   }
+      // }
+    }
+  }
+</script>
