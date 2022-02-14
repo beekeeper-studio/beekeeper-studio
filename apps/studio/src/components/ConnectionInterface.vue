d18633f0ec5c802c82f334116099a19a750e29c3
Matthew Rathbone <matthew.rathbone@gmail.com>
2022-02-14 14:41:44 -0600

    connection interface change

--- apps/studio/src/components/ConnectionInterface.vue
+++ apps/studio/src/components/ConnectionInterface.vue
@@ -22,7 +22,7 @@
                 </select>
               </div>
               <div v-if="config.connectionType">
-                
+
                 <!-- INDIVIDUAL DB CONFIGS -->
                 <postgres-form v-if="config.connectionType === 'cockroachdb'" :config="config" :testing="testing"></postgres-form>
                 <mysql-form v-if="['mysql', 'mariadb'].includes(config.connectionType)" :config="config" :testing="testing" @save="save" @test="testConnection" @connect="submit"></mysql-form>
@@ -45,13 +45,13 @@
             </form>
 
           </div>
-          <div class="pitch"><span class="badge">NEW</span> Share data across devices (or with your team) using <a href="https://www.beekeeperstudio.io/blog/release-3.0">workspaces</a>.</div>
+          <div class="pitch"></div>
           <div v-if="connectionError" class="alert alert-danger">
             {{connectionError}}
           </div>
         </div>
-        
-        <small class="app-version"><a href="https://www.beekeeperstudio.io/releases/latest">Beekeeper Studio {{version}}</a></small>
+
+        <small class="app-version"><a href="https://www.beekeeperstudio.io/releases/latest">Beekeeper Studio Ultimate {{version}}</a></small>
       </div>
     </div>
   </div>
