2317d482107c88e1d99304c9462d06a43596f908
ndom91 <yo@ndo.dev>
2023-03-07 16:41:06 +0100

    fix: import type in client.ts

--- apps/studio/src/lib/db/client.ts
+++ apps/studio/src/lib/db/client.ts
@@ -5,7 +5,7 @@ import createLogger from '../logger';
 import { SSHConnection } from '@/vendor/node-ssh-forward/index';
 import { SupportedFeatures, FilterOptions, TableOrView, Routine, TableColumn, SchemaFilterOptions, DatabaseFilterOptions, TableChanges, TableUpdateResult, OrderBy, TableFilter, TableResult, StreamResults, CancelableQuery, ExtendedTableColumn, PrimaryKeyColumn, TableProperties, TableIndex, TableTrigger, TableInsert } from './models';
 import { AlterTableSpec, IndexAlterations, RelationAlterations } from '@shared/lib/dialects/models';
-import { RedshiftOptions } from '@/common/appdb/models/saved_connection';
+import type { RedshiftOptions } from '@/common/appdb/models/saved_connection';
 
 const logger = createLogger('db');
 
