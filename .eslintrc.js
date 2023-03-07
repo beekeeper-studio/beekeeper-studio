722d8dafc51617459cbd435049a9b188d55c96e2
ndom91 <yo@ndo.dev>
2023-03-07 16:40:16 +0100

    fix: update eslintrc config [sc-00]

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,16 +7,21 @@ module.exports = {
     "es2020": true,
   },
   "ignorePatterns": ["node_modules", "dist", "apps/**/tsconfig.json"],
+  "plugins": ["@typescript-eslint"],
   "extends": [
-    "plugin:vue/essential",
     "eslint:recommended",
-    "@vue/typescript"
+    "plugin:vue/essential",
+    "plugin:vue/strongly-recommended",
+    "plugin:@typescript-eslint/recommended",
+    "@vue/typescript",
+    "@vue/eslint-config-typescript"
   ],
   "rules": {
     "no-console": "off",
     "@typescript-eslint/no-unused-vars": "off",
     "no-unused-vars": "off"
   },
+  "parser": "vue-eslint-parser",
   "parserOptions": {
     "parser": "@typescript-eslint/parser",
     "ecmaFeatures": {
