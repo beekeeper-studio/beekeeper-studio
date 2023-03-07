69d3b3d4ea8ae64251494a6214b0c1a6a1fea2a7
Nico Domino <yo@ndo.dev>
2023-03-07 17:13:25 +0100

    Restore .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,35 +1,35 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true,
+  "root": true,
+  "env": {
+    "node": true,
     // activate “es2020” globals to fix 'BigInt' is not defined
     // https://futurestud.io/tutorials/eslint-how-to-fix-bigint-is-not-defined
-    es2020: true,
+    "es2020": true,
   },
-  ignorePatterns: ["node_modules", "dist", "apps/**/tsconfig.json"],
-  extends: [
+  "ignorePatterns": ["node_modules", "dist", "apps/**/tsconfig.json"],
+  "extends": [
     "plugin:vue/essential",
     "eslint:recommended",
     "@vue/typescript"
   ],
-  rules: {
+  "rules": {
     "no-console": "off",
     "@typescript-eslint/no-unused-vars": "off",
     "no-unused-vars": "off"
   },
-  parserOptions: {
-    parser: "@typescript-eslint/parser",
-    ecmaFeatures: {
+  "parserOptions": {
+    "parser": "@typescript-eslint/parser",
+    "ecmaFeatures": {
       "legacyDecorators": true
     }
   },
-  overrides: [
+  "overrides": [
     {
-      files: [
+      "files": [
         "apps/**/tests/**/*.{j,t}s?(x)"
       ],
-      env: {
-        jest: true
+      "env": {
+        "jest": true
       }
     }
   ]
