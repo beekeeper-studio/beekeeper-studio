b30f6d9d507ce411fe15cd26deacca04667ae063
Matthew Rathbone <matthew.rathbone@gmail.com>
2023-06-10 20:08:06 -0500

    should no longer sign master builds

--- apps/studio/build/win/sign.js
+++ apps/studio/build/win/sign.js
@@ -1,9 +1,24 @@
 const cp = require('child_process')
+
+function isEmpty(value) {
+  return !value || !value.length
+}
+
 exports.default = async function (configuration) {
-  const keyvault = JSON.parse(process.env.KEYVAULT_AUTH)
-  const certificate = process.env.KV_WIN_CERTIFICATE
+
+  const certificate = process.env.KV_WIN_CERTIFICATE;
+  const auth_raw = process.env.KEYVAULT_AUTH;
+  // this way we don't have to sign EVERY build
+  if(isEmpty(certificate) || isEmpty(auth_raw)) {
+    console.warn(`build/sign.js: Cannot sign exe, no KV_WIN_CERTIFICATE/KEYVAULT_AUTH provided for ${configuration.path}`);
+    return null;
+  }
+
+  const keyvault = JSON.parse(auth_raw)
   const timeserver = "http://timestamp.digicert.com"
 
+  // This took me 2 weeks to figure out.
+  // Hi there Matthew in 2026, hope this still works.
   const command = [
     'azuresigntool.exe sign -fd sha384',
     '-kvu', keyvault.url,
@@ -16,6 +31,7 @@ exports.default = async function (configuration) {
     '-v'
   ]
 
+  // throws an error if non-0 exit code, that's what we want.
   cp.execSync(
     `${command.join(" ")} "${configuration.path}"`,
     {
