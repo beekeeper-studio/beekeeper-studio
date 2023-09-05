9f66dacb307c75042a12bbe93d34de9c73bf46eb
azmy60 <38707148+azmy60@users.noreply.github.com>
2023-09-05 15:01:43 +0700

    Hide titlebar on fullscreen

--- apps/studio/src/components/Titlebar.vue
+++ apps/studio/src/components/Titlebar.vue
@@ -1,53 +1,59 @@
 <template>
-  <div
-    class="titlebar"
-    @dblclick.prevent.stop="maximizeWindow"
-    :class="{windows: !$config.isMac}"
-  >
+  <div class="titlebar-wrapper">
     <div
-      class="titlebar-icon"
-      v-if="!$config.isMac"
-    >
-      <img src="@/assets/logo.svg">
-      <AppMenu />
-    </div>
-    <div class="titlebar-title noselect">
-      {{ windowTitle }}
-    </div>
+      class="titlebar-reveal"
+      v-show="fullscreen"
+    />
     <div
-      class="titlebar-actions"
-      v-if="!$config.isMac"
+      class="titlebar"
+      @dblclick.prevent.stop="maximizeWindow"
+      :class="{ windows: !$config.isMac, fullscreen }"
     >
-      <template>
-        <button
-          class="btn btn-link"
-          id="minimize"
-          @click.prevent="minimizeWindow"
-        >
-          <i class="material-icons">remove</i>
-        </button>
-        <button
-          class="btn btn-link"
-          id="maximize"
-          @click.prevent="maximizeWindow"
-        >
-          <i
-            class="material-icons maximized"
-            v-if="maximized"
-          >filter_none</i>
-          <i
-            class="material-icons"
-            v-else
-          >crop_square</i>
-        </button>
-        <button
-          class="btn btn-link"
-          id="quit"
-          @click.prevent="closeWindow"
-        >
-          <i class="material-icons">clear</i>
-        </button>
-      </template>
+      <div
+        class="titlebar-icon"
+        v-if="!$config.isMac"
+      >
+        <img src="@/assets/logo.svg">
+        <AppMenu />
+      </div>
+      <div class="titlebar-title noselect">
+        {{ windowTitle }}
+      </div>
+      <div
+        class="titlebar-actions"
+        v-if="!$config.isMac"
+      >
+        <template>
+          <button
+            class="btn btn-link"
+            id="minimize"
+            @click.prevent="minimizeWindow"
+          >
+            <i class="material-icons">remove</i>
+          </button>
+          <button
+            class="btn btn-link"
+            id="maximize"
+            @click.prevent="maximizeWindow"
+          >
+            <i
+              class="material-icons maximized"
+              v-if="maximized"
+            >filter_none</i>
+            <i
+              class="material-icons"
+              v-else
+            >crop_square</i>
+          </button>
+          <button
+            class="btn btn-link"
+            id="quit"
+            @click.prevent="closeWindow"
+          >
+            <i class="material-icons">clear</i>
+          </button>
+        </template>
+      </div>
     </div>
   </div>
 </template>
@@ -61,6 +67,7 @@ export default {
   data() {
     return {
       maximized: this.$native.getCurrentWindow()?.isMaximized(),
+      fullscreen: this.$native.getCurrentWindow()?.isFullScreen(),
     }
   },
   computed: {
@@ -70,9 +77,14 @@ export default {
     this.getWindow()?.on('maximize', () => {
       this.maximized = true
     })
-
     this.getWindow()?.on('unmaximize', () => {
       this.maximized = false
+    })
+    this.getWindow()?.on('enter-full-screen', () => {
+      this.fullscreen = true
+    })
+    this.getWindow()?.on('leave-full-screen', () => {
+      this.fullscreen = false
     })
   },
   methods: {
