e1476a96ffdbbc19980b9279e2467221e7a87af6
azmy60 <mazmiagus2003@gmail.com>
2023-09-28 22:22:31 +0700

    simplify values

--- apps/studio/src/components/tableview/TableTable.vue
+++ apps/studio/src/components/tableview/TableTable.vue
@@ -656,10 +656,7 @@ export default Vue.extend({
         if (/^(bool|boolean)$/i.test(column.dataType)) {
           const trueVal = this.dialectData.boolean?.true ?? true
           const falseVal = this.dialectData.boolean?.false ?? false
-          const values = [
-            { label: trueVal.toString(), value: trueVal },
-            { label: falseVal.toString(), value: falseVal },
-          ]
+          const values = [falseVal, trueVal]
           if (column.nullable) values.push({ label: '(NULL)', value: null })
           result.editorParams['values'] = values
         }
