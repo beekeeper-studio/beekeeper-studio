4312051a0e34ddf64d95422b95f5a2b564cecd2e
azmy60 <mazmiagus2003@gmail.com>
2023-09-29 22:51:10 +0700

    debouncing filter builder input

--- apps/studio/src/components/tableview/TableTable.vue
+++ apps/studio/src/components/tableview/TableTable.vue
@@ -12,7 +12,7 @@
         v-if="table.columns?.length"
         :columns="table.columns"
         :initial-filters="initialFilters"
-        @changed="saveFilters"
+        @input="handleRowFilterBuilderInput"
         @submit="triggerFilter"
       />
       <div ref="table" />
@@ -1587,10 +1587,13 @@ export default Vue.extend({
       this.triggerFilter(draftFilters);
       this.$modal.hide(`discard-changes-modal-${this.tab.id}`);
     },
-    saveFilters(filters: TableFilter[]) {
+    handleRowFilterBuilderInput(filters: TableFilter[]) {
       this.tab.setFilters(filters)
-      this.$store.dispatch('tabs/save', this.tab)
+      this.debouncedSaveTab(this.tab)
     },
+    debouncedSaveTab: _.debounce(function(tab) {
+      this.$store.dispatch('tabs/save', tab)
+    }, 300),
   }
 });
 </script>
