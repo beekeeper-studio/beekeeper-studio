7a85e983e02cad69dd60806e39d37835429508af
Fawzi Abdulfattah <iifawzie@gmail.com>
2023-09-29 17:09:47 +0300

    feat: honoring compound foreign keys in table structure and tables navigation
    - compound foreign keys will be shown as a single unit in the table structure
    - honoring compound foreign keys while navigating to the referenced table
    - fixes #1714

--- apps/studio/src/lib/db/clients/postgresql.ts
+++ apps/studio/src/lib/db/clients/postgresql.ts
@@ -980,35 +980,47 @@ export async function getTableReferences(conn: Conn, table: string, schema: stri
 
 export async function getTableKeys(conn: Conn, _database: string, table: string, schema: string): Promise<TableKey[]> {
   const sql = `
-SELECT
-    kcu.constraint_schema AS from_schema,
-    kcu.table_name AS from_table,
-    kcu.column_name AS from_column,
-    rc.unique_constraint_schema AS to_schema,
-    tc.constraint_name,
-    rc.update_rule,
-    rc.delete_rule,
-    (SELECT kcu2.table_name
-     FROM information_schema.key_column_usage AS kcu2
-     WHERE kcu2.constraint_name = rc.unique_constraint_name) AS to_table,
-    (SELECT kcu2.column_name
-     FROM information_schema.key_column_usage AS kcu2
-     WHERE kcu2.constraint_name = rc.unique_constraint_name) AS to_column
+  SELECT
+  kcu.constraint_schema AS from_schema,
+  kcu.table_name AS from_table,
+  STRING_AGG(kcu.column_name, ',' ORDER BY kcu.ordinal_position) AS from_column,
+  rc.unique_constraint_schema AS to_schema,
+  tc.constraint_name,
+  rc.update_rule,
+  rc.delete_rule,
+  (
+      SELECT STRING_AGG(kcu2.column_name, ',' ORDER BY kcu2.ordinal_position)
+      FROM information_schema.key_column_usage AS kcu2
+      WHERE kcu2.constraint_name = rc.unique_constraint_name
+  ) AS to_column,
+  (
+      SELECT kcu2.table_name
+      FROM information_schema.key_column_usage AS kcu2
+      WHERE kcu2.constraint_name = rc.unique_constraint_name LIMIT 1
+  ) AS to_table
 FROM
-    information_schema.key_column_usage AS kcu
+  information_schema.key_column_usage AS kcu
 JOIN
-    information_schema.table_constraints AS tc
+  information_schema.table_constraints AS tc
 ON
-    tc.constraint_name = kcu.constraint_name
+  tc.constraint_name = kcu.constraint_name
 JOIN
-    information_schema.referential_constraints AS rc
+  information_schema.referential_constraints AS rc
 ON
-    rc.constraint_name = kcu.constraint_name
+  rc.constraint_name = kcu.constraint_name
 WHERE
-    tc.constraint_type = 'FOREIGN KEY' AND
-    kcu.table_schema NOT LIKE 'pg_%' AND
-    kcu.table_schema = $2 AND
-    kcu.table_name = $1;
+  tc.constraint_type = 'FOREIGN KEY' AND
+  kcu.table_schema NOT LIKE 'pg_%' AND
+  kcu.table_schema = $2 AND
+  kcu.table_name = $1
+GROUP BY
+  kcu.constraint_schema,
+  kcu.table_name,
+  rc.unique_constraint_schema,
+  rc.unique_constraint_name,
+  tc.constraint_name,
+  rc.update_rule,
+  rc.delete_rule;
 `;
 
   const params = [
