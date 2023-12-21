47865c0a233c6061d3ff0a1c05e5c7bba53ecee3
Will Montgomery <wmontgomery@budgetdumpster.com>
2023-12-20 23:41:11 -0500

    feat: show the date as an iso string in tooltip if an integer

--- apps/studio/src/mixins/data_mutators.ts
+++ apps/studio/src/mixins/data_mutators.ts
@@ -33,6 +33,10 @@ function emptyResult(value: any) {
   return null
 }
 
+function buildFormatterWithTooltip(cellValue: string, tooltip: string, icon: string) {
+  return `<div class="cell-link-wrapper">${cellValue}<i class="material-icons fk-link" title="${tooltip}">${icon}</i></div>`
+}
+
 export default {
 
   methods: {
@@ -43,7 +47,7 @@ export default {
     },
     cellFormatter(
       cell: Tabulator.CellComponent,
-      params: { fk?: any[], fkOnClick?: (e: MouseEvent, cell: Tabulator.CellComponent) => void },
+      params: { fk?: any[], isPK?: boolean, fkOnClick?: (e: MouseEvent, cell: Tabulator.CellComponent) => void },
       onRendered: (func: () => void) => void
     ) {
       const nullValue = emptyResult(cell.getValue())
@@ -53,22 +57,25 @@ export default {
       let cellValue = this.niceString(cell.getValue(), true)
       cellValue = cellValue.replace(/\n/g, ' ↩ ');
       cellValue = escapeHtml(cellValue);
-
+      
       // removing the <pre> will break selection / copy paste, see ResultTable
       let result = `<pre>${cellValue}</pre>`
+      let tooltip = ''
 
-      if (params?.fk) {
-        let tooltip = ''
+      if (params.fk) {
         if (params.fk.length === 1) tooltip = `View record in ${params.fk[0].toTable}`
         else tooltip = `View records in ${(params.fk.map(item => item.toTable).join(', ') as string).replace(/, (?![\s\S]*, )/, ', or ')}`
 
-        result = `<div class="cell-link-wrapper">${cellValue}<i class="material-icons fk-link" title="${tooltip}">launch</i></div>`
+        result = buildFormatterWithTooltip(cellValue, tooltip, 'launch')
 
         onRendered(() => {
           const fkLink = cell.getElement().querySelector('.fk-link') as HTMLElement
           fkLink.onclick = (e) => params.fkOnClick(e, cell);
         })
-      }
+      } else if ( Object.prototype.hasOwnProperty.call(params, 'isPK') && !params.isPK && _.isInteger(Number(cellValue)) && Number(cellValue) <= Number.MAX_SAFE_INTEGER) {
+        tooltip = new Date(Number(cellValue)).toISOString()
+        result = buildFormatterWithTooltip(cellValue, tooltip, 'timelapse')
+    }
 
       return result;
     },
