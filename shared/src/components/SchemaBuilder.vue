1bd0619e89f09d16b74ced492454285c9eb0f35e
Mohammad Azmi <mazmiagus2003@gmail.com>
2024-01-06 18:32:08 +0700

    fix column name style in table structure view

--- shared/src/components/SchemaBuilder.vue
+++ shared/src/components/SchemaBuilder.vue
@@ -318,6 +318,7 @@ export default Vue.extend({
       &.tabulator-row-even,
       &:nth-child(odd) {
         background: rgba($theme-base, 0.05);
+        --row-bg-color: #{rgba($theme-base, 0.05)};
       }
       .tabulator-cell {
         min-height: $row-height;
@@ -376,6 +377,8 @@ export default Vue.extend({
         // Read Only
         &.read-only,
         &.read-only:hover {
+          --row-bg-color: transparent;
+          --row-hover-bg-color: transparent;
           background: transparent!important;
           cursor: default;
           input {
