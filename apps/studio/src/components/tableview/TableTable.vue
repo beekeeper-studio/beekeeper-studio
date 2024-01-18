59acf83fb3cb998ad0619fd6143abfef6e90c88e
Mohammad Azmi <mazmiagus2003@gmail.com>
2024-01-18 20:43:06 +0700

    tabulator can set the width of the column header

--- apps/studio/src/components/tableview/TableTable.vue
+++ apps/studio/src/components/tableview/TableTable.vue
@@ -499,7 +499,7 @@ export default Vue.extend({
         const hasKeyDatas = keyDatas && keyDatas.length > 0
         const columnWidth = this.table.columns.length > 30 ?
           this.defaultColumnWidth(slimDataType, globals.bigTableColumnWidth) :
-          this.countInitialColumnWidth(column.columnName, column.dataType, isPK || hasKeyDatas);
+          undefined;
 
         let headerTooltip = `${column.columnName} ${column.dataType}`
         if (hasKeyDatas) {
@@ -934,18 +934,6 @@ export default Vue.extend({
       const chunkyTypes = ['json', 'jsonb', 'blob', 'text', '_text', 'tsvector']
       if (chunkyTypes.includes(slimType)) return globals.largeFieldWidth
       return defaultValue
-    },
-    countInitialColumnWidth(columnName: string, dataType: string, isKey: boolean) {
-      const charWidth = 5 // approximately
-      const keyWidth = 20 // approximately
-      const headerWithoutTitle = 76 // approximately
-      const offset = 10
-      const titleWidth = (columnName.length + dataType.length) * charWidth
-      let initialWidth = headerWithoutTitle + titleWidth + offset
-      if (isKey) {
-        initialWidth += keyWidth
-      }
-      return initialWidth
     },
     // TODO: this is not attached to anything. but it might be needed?
     allowHeaderSort(column) {
