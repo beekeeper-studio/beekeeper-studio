c201f0e4ba131f4691328321dfdfd7945762b3c6
Matthew Rathbone <matthew.rathbone@gmail.com>
2024-03-29 14:16:47 -0500

    node 16 is special

--- apps/studio/jest.polyfills.js
+++ apps/studio/jest.polyfills.js
@@ -9,7 +9,7 @@
  * you don't want to deal with this.
  */
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const { TextDecoder, TextEncoder } = require('node:util')
+const { TextDecoder, TextEncoder } = require('util')
 
 // eslint-disable-next-line no-undef
 Object.defineProperties(globalThis, {
