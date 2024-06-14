eec583e2e54fc97cda3bf35e9079e6a7ebb294d2
Matthew Rathbone <matthew.rathbone@gmail.com>
2024-06-14 16:32:41 -0500

    bump

--- .github/scripts/delete_latest_yml.js
+++ .github/scripts/delete_latest_yml.js
@@ -1,38 +1,42 @@
 
-module.exports = async ({ github, context, core }, owner, repo, releaseId) => {
+module.exports = async ({ github, core }, assetsUrl) => {
   const assetName = 'latest-mac.yml';
 
-  async function findAssetIdByName(assets, name) {
+  async function findAssetByName(assets, name) {
     for (const asset of assets) {
       if (asset.name === name) {
-        return asset.id;
+        return asset;
       }
     }
     return null;
   }
 
   try {
-    // Get the existing assets for the release
-    const { data: assets } = await github.rest.repos.listReleaseAssets({
-      owner: owner,
-      repo: repo,
-      release_id: releaseId,
+    // Get the existing assets for the release using the assetsUrl
+    const { data: assets } = await github.request({
+      method: 'GET',
+      url: assetsUrl,
+      headers: {
+        accept: 'application/vnd.github.v3+json',
+      },
     });
 
     // Check if the asset already exists and delete it if necessary
-    const assetId = await findAssetIdByName(assets, assetName);
-    if (assetId) {
-      await github.rest.repos.deleteReleaseAsset({
-        owner: owner,
-        repo: repo,
-        asset_id: assetId,
+    const asset = await findAssetByName(assets, assetName);
+    if (asset) {
+      await github.request({
+        method: 'DELETE',
+        url: asset.url,
+        headers: {
+          accept: 'application/vnd.github.v3+json',
+        },
       });
       core.info(`Deleted existing asset: ${assetName}`);
     } else {
       core.info(`No existing asset found with name: ${assetName}`);
     }
   } catch (error) {
-    core.error('Error deleting asset:', error);
-    core.setFailed(`Action failed with error ${error}`);
+    core.error(`Error deleting asset: ${error.message}`);
+    core.setFailed(`Action failed with error ${error.message}`);
   }
 };
