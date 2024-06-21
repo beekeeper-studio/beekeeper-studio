9ea31b26de88459ba7fca8d93a5fd3cbe5d6a9d9
Matthew Rathbone <matthew.rathbone@gmail.com>
2024-06-21 14:06:37 -0500

    much faster tests

--- apps/studio/jest.config.js
+++ apps/studio/jest.config.js
@@ -11,6 +11,7 @@ module.exports = {
     "^axios$": "axios/dist/node/axios.cjs",
     '^@libsql/core/(.*)': resolve(__dirname, '../../node_modules/@libsql/core/lib-cjs/$1'),
   },
+  maxWorkers: 1,
 
   moduleFileExtensions: [
     'js',
@@ -48,7 +49,7 @@ module.exports = {
     '.+\\.(css|styl|less|sass|scss|jpg|jpeg|png|svg|gif|eot|otf|webp|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga|avif)$':
       require.resolve('jest-transform-stub'),
     '^.+\\.jsx?$': require.resolve('babel-jest'),
-    '^.+\\.tsx?$': [require.resolve('ts-jest'), { babelConfig: true}]
+    '^.+\\.tsx?$': [require.resolve('ts-jest'), { babelConfig: true, isolatedModules: true}]
   },
   setupFilesAfterEnv: [resolve(__dirname, './tests/setupTests.js')],
   setupFiles: ['./jest.polyfills.js'],
