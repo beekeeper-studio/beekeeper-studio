ad9f07e39cca74d599dece3df33c7ecb824b3d34
azmy60 <mazmiagus2003@gmail.com>
2024-08-02 23:04:56 +0700

    fix community-4 sqlserver docker test

--- apps/studio/tests/integration/lib/db/clients/sqlserver.spec.ts
+++ apps/studio/tests/integration/lib/db/clients/sqlserver.spec.ts
@@ -6,12 +6,14 @@ import { IDbConnectionServerConfig } from '@/lib/db/types'
 const TEST_VERSIONS = [
   { version: '2017-latest', readonly: false },
   { version: '2017-latest', readonly: true },
-  { version: '2019-latest', readonly: false },
-  { version: '2019-latest', readonly: true },
   // FIXME 2022-latest has a breaking change. We'll use the previous build
   // for now.
+  // { version: '2019-latest', readonly: false },
+  // { version: '2019-latest', readonly: true },
   // { version: '2022-latest', readonly: false },
   // { version: '2022-latest', readonly: true },
+  { version: '2019-CU27-ubuntu-20.04', readonly: false },
+  { version: '2019-CU27-ubuntu-20.04', readonly: true },
   { version: '2022-CU13-ubuntu-22.04', readonly: false },
   { version: '2022-CU13-ubuntu-22.04', readonly: true },
 ]
