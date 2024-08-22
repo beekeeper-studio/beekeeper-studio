25b60e3756b411c0c137339e82e9a72ded410737
Will Montgomery <wmontgomery@budgetdumpster.com>
2024-08-21 22:24:49 -0400

    fix: remove .only and uncomment sqlserver types

--- apps/studio/tests/integration/lib/db/clients/sqlserver.spec.ts
+++ apps/studio/tests/integration/lib/db/clients/sqlserver.spec.ts
@@ -2,7 +2,6 @@ import { GenericContainer, Wait } from 'testcontainers'
 import { DBTestUtil, dbtimeout } from '../../../../lib/db'
 import { runCommonTests, runReadOnlyTests } from './all'
 import { IDbConnectionServerConfig } from '@/lib/db/types'
-import { TableOrView } from '@/lib/db/models'
 
 const TEST_VERSIONS = [
   { version: '2017-latest', readonly: false },
@@ -14,9 +13,9 @@ const TEST_VERSIONS = [
   // { version: '2022-latest', readonly: false },
   // { version: '2022-latest', readonly: true },
   { version: '2019-CU27-ubuntu-20.04', readonly: false },
-  // { version: '2019-CU27-ubuntu-20.04', readonly: true },
-  // { version: '2022-CU13-ubuntu-22.04', readonly: false },
-  // { version: '2022-CU13-ubuntu-22.04', readonly: true },
+  { version: '2019-CU27-ubuntu-20.04', readonly: true },
+  { version: '2022-CU13-ubuntu-22.04', readonly: false },
+  { version: '2022-CU13-ubuntu-22.04', readonly: true },
 ]
 
 function testWith(dockerTag: string, readonly: boolean) {
@@ -136,71 +135,6 @@ function testWith(dockerTag: string, readonly: boolean) {
       expect(secondResult).toStrictEqual({
         id: 2,
         bitcol: true
-      })
-    })
-
-    // import tests keep timing out for some reason, but they do work in ultimate when running
-    // Not sure what's up with that
-    describe.skip("Imports", () => {
-      it('should import correctly', async () => {
-        const tableName = 'import_table'
-        const executeOptions = { multiple: false }
-        const table = {
-          name: tableName,
-          schema: 'public',
-          entityType: 'table'
-        } as TableOrView
-        const {
-          step0,
-          beginCommand,
-          truncateCommand,
-          lineReadCommand,
-          commitCommand,
-          rollbackCommand,
-          finalCommand
-          } = util.connection.getImportScripts(table)
-        const formattedData = util.buildImportData(tableName)
-        const importSQL = util.connection.getImportSQL(formattedData)
-
-        expect(typeof step0).toBe('function')
-        expect(typeof beginCommand).toBe('function')
-        expect(typeof truncateCommand).toBe('function')
-        expect(typeof lineReadCommand).toBe('function')
-        expect(typeof commitCommand).toBe('function')
-        expect(typeof rollbackCommand).toBe('function')
-        expect(finalCommand).toBeUndefined()
-
-        await step0(executeOptions)
-        await beginCommand(executeOptions)
-        await truncateCommand(executeOptions)
-        await lineReadCommand(importSQL, {multiple: true})
-        await commitCommand(executeOptions)
-
-        const hats = await util.knex.select().table(tableName)
-        expect(hats.length).toBe(4)
-      })
-
-      it('should rollback', async () => {
-        const tableName = 'import_table'
-        const executeOptions = { multiple: false }
-        const table = {
-          name: tableName,
-          entityType: 'table'
-        } as TableOrView
-        const formattedData = util.buildImportData(tableName)
-        const {
-          beginCommand,
-          lineReadCommand,
-          rollbackCommand,
-        } = util.connection.getImportScripts(table)
-        const importSQL = util.connection.getImportSQL(formattedData, ['id'])
-        const hatsStart = await util.knex.select().table(tableName)
-        await beginCommand(executeOptions)
-        await lineReadCommand(importSQL, {multiple: true})
-        await rollbackCommand(executeOptions)
-
-        const hats = await util.knex.select().table(tableName)
-        expect(hats.length).toBe(hatsStart.length)
       })
     })
   })
