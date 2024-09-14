0525c72431e4ba40d8861fc42457d3bff8a05a39
Matthew Rathbone <matthew.rathbone@gmail.com>
2024-09-13 22:33:42 -0500

    more upgrades

--- apps/studio/tests/unit/shared/lib/dialects/sqlserver.spec.js
+++ apps/studio/tests/unit/shared/lib/dialects/sqlserver.spec.js
@@ -4,8 +4,8 @@ import { SqlServerData } from "@/shared/lib/dialects/sqlserver"
 describe("SQL Server Dialect data", () => {
   test("should wrapIdentifiers with proper escaping", () => {
     const testCases = [
-      ["my[socks]", "[my[[socks]]"],
-      ["[foobar]", "[[[foobar]]"]
+      ["my[socks]", "[my[socks]]]"],
+      ["[foobar]", "[[foobar]]]"]
     ]
     testCases.forEach(([i, exp]) => {
       expect(SqlServerData.wrapIdentifier(i)).toEqual(exp)
