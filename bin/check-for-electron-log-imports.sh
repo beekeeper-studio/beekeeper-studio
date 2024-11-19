4872e7bf6631c48f8f3c1ece674bdebc9d24717a
Day Matchullis <dsm6069@gmail.com>
2024-11-19 14:25:31 -0700

    check for direct imports of logger files as well

--- bin/check-for-electron-log-imports.sh
+++ bin/check-for-electron-log-imports.sh
@@ -7,8 +7,17 @@ if [[ $STATUS -eq 0 ]]
 then
   echo "We no longer use electron-log directly, please import from '@bksLogger'"
   exit 1;
+fi
 
+git grep -a \( -e mainLogger -e rendererLogger -e utilityLogger \) apps/studio/src* ':!*bksLogger.ts'
+
+STATUS=$?
+
+if [[ $STATUS -eq 0 ]]
+then
+  echo "Please don't import from mainLogger.ts, rendererLogger.ts, or utilityLogger.ts. Use '@bksLogger' instead"
+  exit 1;
 else
-  echo "No bad log imports"
-  exit 0
+  echo "No bad imports"
+  exit 0;
 fi
