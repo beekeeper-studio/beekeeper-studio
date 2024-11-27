857822e586a088fb38153962999500e9d69b7c7e
Matthew Rathbone <matthew.rathbone@gmail.com>
2024-11-27 10:58:15 -0600

    mono v1

---
+++ apps/ui-kit/lib/main.ts
@@ -0,0 +1,7 @@
+
+
+
+
+export const Example = {
+  foo: 'bar'
+}
---
+++ apps/ui-kit/vite.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue2';
+import { resolve } from 'path'
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'lib/main.ts'),
+      formats: ['es']
+    }
+  }
+
+})
