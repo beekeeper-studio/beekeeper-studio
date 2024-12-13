538e2d27b8663ab78dd3bc87ed1f1aa0e480960b
azmy60 <mazmiagus2003@gmail.com>
2024-12-13 20:13:39 +0700

    add text editor to react example

--- apps/ui-kit/examples/react/README.md
+++ apps/ui-kit/examples/react/README.md
@@ -1,6 +1,17 @@
-# React + Vite
+# React + Vite + @bks/ui-kit
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+## Quickstart
+
+```bash
+cd apps/ui-kit
+yarn lib:dev
+
+# open a second terminal
+cd apps/ui-kit/examples/react
+yarn dev
+```
+
+This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules, and the Beekeeper UI Kit!.
 
 Currently, two official plugins are available:
 
--- apps/ui-kit/examples/react/src/App.jsx
+++ apps/ui-kit/examples/react/src/App.jsx
@@ -1,11 +1,43 @@
-import { useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import bksLogo from './assets/bks.svg'
+import '@bks/ui-kit/components/SqlTextEditor'
+import '@bks/ui-kit/style.css'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [text, setText] = useState('select * from users u\nwhere u.id = 1;')
+  const [tables, setTables] = useState({
+    users: ['id', 'name', 'email'],
+    posts: ['id', 'title', 'body'],
+    comments: ['id', 'comment', 'post_id', 'user_id'],
+  })
+  const sqlTextEditorRef = useRef(null)
+
+  useEffect(() => {
+    const handleUpdateValue = (event) => {
+      const updatedText = event.detail[0];
+      if (text === updatedText) return
+      setText(updatedText);
+    };
+
+    const webComponent = sqlTextEditorRef.current;
+
+    if (webComponent) {
+      webComponent.addEventListener("update:value", handleUpdateValue);
+    }
+
+    return () => {
+      if (webComponent) {
+        webComponent.removeEventListener("update:value", handleUpdateValue);
+      }
+    };
+  }, [])
+
+  useEffect(() => {
+    sqlTextEditorRef.current.hintOptions = { tables }
+  }, [tables])
 
   return (
     <>
@@ -23,7 +55,7 @@ function App() {
       <h1>Vite + React + @bks/ui-kit</h1>
       <div className="card">
         <h2>Sql Text Editor</h2>
-        <bks-sql-text-editor />
+        <bks-sql-text-editor ref={sqlTextEditorRef} value={text} focus />
         <h2>Table</h2>
         <bks-table />
         <h2>Table List</h2>
