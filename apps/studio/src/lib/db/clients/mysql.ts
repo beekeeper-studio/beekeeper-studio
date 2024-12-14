ab1d0aef01eb6ad21dbb07ba1501704b146b02a2
Day Matchullis <dsm6069@gmail.com>
2024-12-13 17:41:14 -0700

    fix mysql binary parsing the wrong columns

--- apps/studio/src/lib/db/clients/mysql.ts
+++ apps/studio/src/lib/db/clients/mysql.ts
@@ -618,7 +618,7 @@ export class MysqlClient extends BasicDatabaseClient<ResultType> {
     );
     const { query, params } = queries;
     const result = await this.driverExecuteSingle(query, { params });
-    const fields = this.parseQueryResultColumns(result);
+    const fields = columns.map((v) => v.bksField).filter((v) => selects && selects.length > 0 ? selects.includes(v.name) : true);
     const rows = await this.serializeQueryResult(result, fields);
     return { result: rows, fields };
   }
