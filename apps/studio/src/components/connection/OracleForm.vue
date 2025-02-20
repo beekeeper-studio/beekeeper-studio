273408d0957b200a0889397825fffe1b9d86dc39
Matthew Rathbone <matthew.rathbone@gmail.com>
2025-02-20 09:30:29 -0600

    Oracle fixes and updates to use the thin-client
    - Oracle now has two operation modes - thick and thin
    - Thin mode requires NO NATIVE CODE, we like this a lot
    - Also we had some bugs where we didn't release connections back to the pool. These are fixed
    - Removed warnings and updated docs

--- apps/studio/src/components/connection/OracleForm.vue
+++ apps/studio/src/components/connection/OracleForm.vue
@@ -1,128 +1,109 @@
 <template>
   <div class="with-connection-type oracledb-form">
-    <template v-if="$config.oracleSupported">
-      <div class="advanced-connection-settings">
-        <div class="flex flex-middle">
-          <span
-            class="btn-link btn-fab"
-            @click.prevent="oracleExpanded = !oracleExpanded"
-          >
-            <i
-              v-if="!oracleExpanded"
-              class="material-icons"
-            >keyboard_arrow_right</i>
-            <i
-              v-if="oracleExpanded"
-              class="material-icons"
-            >keyboard_arrow_down</i>
-          </span>
-          <h4 class="advanced-heading">
-            Global Oracle Configuration
-          </h4>
-        </div>
-        <div
-          v-if="oracleExpanded"
-          class="advanced-body"
+    <div class="advanced-connection-settings">
+      <div class="flex flex-middle">
+        <span
+          class="btn-link btn-fab"
+          @click.prevent="oracleExpanded = !oracleExpanded"
         >
-          <settings-input
-            setting-key="oracleInstantClient"
-            input-type="directory"
-            @changed="restart"
-            title="Instant Client Location"
-            :help="help"
-          />
-          <settings-input
-            setting-key="oracleConfigLocation"
-            input-type="directory"
-            title="TNS_ADMIN override"
-            help="The directory containing tnsnames.ora, sqlnet.ora, and wallets"
-          />
-        </div>
+          <i
+            v-if="!oracleExpanded"
+            class="material-icons"
+          >keyboard_arrow_right</i>
+          <i
+            v-if="oracleExpanded"
+            class="material-icons"
+          >keyboard_arrow_down</i>
+        </span>
+        <h4 class="advanced-heading">
+          Global Oracle Configuration (optional)
+        </h4>
       </div>
-
-      <div class="form-group">
-        <label for="connectionType">Connection Method</label>
-        <select v-model="connectionMethod">
-          <option value="manual">
-            Manual Host and Port
-          </option>
-          <option value="connectionString">
-            Connection String or Alias
-          </option>
-        </select>
-      </div>
-
       <div
-        class="oracle-manual"
-        v-if="connectionMethod === 'manual'"
+        v-if="oracleExpanded"
+        class="advanced-body"
       >
-        <common-server-inputs
-          :support-complex-s-s-l="false"
-          ssl-help="Requires your wallet to be already set up in TNS_ADMIN"
-          :config="config"
+        <!-- 5.1 - disabling instant client, keeping the driver in 'thin' mode -->
+        <!-- <settings-input
+          setting-key="oracleInstantClient"
+          input-type="directory"
+          @changed="restart"
+          title="Instant Client Location"
+          :help="help"
+        /> -->
+        <settings-input
+          setting-key="oracleConfigLocation"
+          input-type="directory"
+          title="TNS_ADMIN override"
+          help="The directory containing tnsnames.ora, sqlnet.ora, and wallets"
         />
-        <div class="form-group">
-          <label for="serviceName">Service Name</label>
+      </div>
+    </div>
+
+    <div class="form-group">
+      <label for="connectionType">Connection Method</label>
+      <select v-model="connectionMethod">
+        <option value="manual">
+          Manual Host and Port
+        </option>
+        <option value="connectionString">
+          Connection String or Alias
+        </option>
+      </select>
+    </div>
+
+    <div
+      class="oracle-manual"
+      v-if="connectionMethod === 'manual'"
+    >
+      <common-server-inputs
+        :support-complex-s-s-l="false"
+        ssl-help="Requires your wallet to be already set up in TNS_ADMIN"
+        :config="config"
+      />
+      <div class="form-group">
+        <label for="serviceName">Service Name</label>
+        <input
+          type="text"
+          class="form-control"
+          v-model="config.serviceName"
+        >
+      </div>
+      <common-advanced :config="config" />
+    </div>
+    <div v-if="connectionMethod === 'connectionString'">
+      <div class="form-group gutter">
+        <label for="connectionString">Connection String or TNS alias</label>
+        <textarea
+          v-model="config.options.connectionString"
+          name="connectionString"
+          class="form-control"
+          id=""
+          cols="30"
+          rows="5"
+        />
+      </div>
+      <div class="row gutter">
+        <div class="col s6 form-group">
+          <label for="user">User (optional)</label>
           <input
             type="text"
+            name="user"
+            v-model="config.username"
             class="form-control"
-            v-model="config.serviceName"
           >
         </div>
-        <common-advanced :config="config" />
-      </div>
-      <div v-if="connectionMethod === 'connectionString'">
-        <div class="form-group gutter">
-          <label for="connectionString">Connection String or TNS alias</label>
-          <textarea
-            v-model="config.options.connectionString"
-            name="connectionString"
+        <div class="col s6 form-group">
+          <label for="password">Password (optional)</label>
+          <input
+            type="password"
+            v-model="config.password"
             class="form-control"
-            id=""
-            cols="30"
-            rows="5"
-          />
+          >
         </div>
-        <div class="row gutter">
-          <div class="col s6 form-group">
-            <label for="user">User (optional)</label>
-            <input
-              type="text"
-              name="user"
-              v-model="config.username"
-              class="form-control"
-            >
-          </div>
-          <div class="col s6 form-group">
-            <label for="password">Password (optional)</label>
-            <input
-              type="password"
-              v-model="config.password"
-              class="form-control"
-            >
-          </div>
-        </div>
-        <br>
       </div>
-    </template>
-    <template v-else>
-      <div class="alert alert-warning">
-        <i class="material-icons-outlined">warning</i>
-        <div>Oracle does not currently support MacOS devices running Apple silicon.</div>
-      </div>
-      <div>
-        <p>Unfortunately, Oracle does not publish a version of the 'Oracle Instant Client' for Apple Silicon.</p>
-        <p class="">
-          Please download the Intel version of the <a
-            href="https://www.oracle.com/database/technologies/instant-client.html"
-            class=""
-          >Oracle Instant Client</a>, and use the <a
-            href="https://beekeeperstudio.io/get"
-            class=""
-          >Intel build of Beekeeper Studio</a> in order to connect to an Oracle Database.
-        </p>
-      </div>
-    </template>
+      <br>
+    </div>
   </div>
 </template>
 <script lang="ts">
@@ -148,6 +129,14 @@ export default Vue.extend({
       },
       get() {
         return this.config.options.connectionMethod
+      }
+    },
+    connectionString: {
+      set(value: string) {
+        this.config.options = { ...this.config.options, connectionString: value }
+      },
+      get() {
+        return this.config.options.connectionString
       }
     },
     restartNotification() {
--- docs/user_guide/connecting/oracle-database.md
+++ docs/user_guide/connecting/oracle-database.md
@@ -8,7 +8,6 @@ old_url: "https://docs.beekeeperstudio.io/docs/oracle-database"
 
 1. On all operating systems you must have the Oracle Instant Client installed.
 2. On Linux you must have `libaio` installed.
-3. On MacOS you must be using the `Intel` version of Beekeeper Studio. This is because Oracle does not publish an `Apple Silicon` version of the Instant Client.
 
 Below are specific instructions for each of the above requirements
 
@@ -37,17 +36,9 @@ There are a number of ways you can connect to an Oracle database using Beekeeper
 2. TSA alias
 3. Host and port
 
+## Using tnsnames.ora
 
-### Before You Connect: Set Instant Client Location
-
-Before you can connect to Oracle at all you need to tell Beekeeper where on your computer the Oracle Instant Client libraries are stored.
-
-This is an Oracle requirement and not something Beekeeper Studio controls.
-
-In Beekeeper Studio you can choose the location of the instant client in the `Global Configuration` section before making your first connection:
-
-![Set oracle instant client](../../assets/images/oracle-database-62.png)
-
+You can specify your 'config' directory when adding an Oracle connection. Beekeeper Studio will use this to find your tnsnames.ora file, you can then use an alias in your connection string.
 
 ### Enter Your Oracle Connection String
 
