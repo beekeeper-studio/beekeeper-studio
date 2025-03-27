06b8109bc898ea5bc94dc114cbd204dc14308f6b
Tiago Dias <tiago.santos.dias@tecnico.ulisboa.pt>
2025-03-27 01:15:44 +0000

    Fix #2697: UI Issues due to long connection names
    Fixed the overflow problem using CSS.

--- apps/studio/src/store/index.ts
+++ apps/studio/src/store/index.ts
@@ -409,16 +409,9 @@ const store = new Vuex.Store<State>({
 
     updateWindowTitle(context) {
       const config = context.state.usedConfig
-      const maxLength = 30
-      const suffix = ' - Beekeeper Studio'
-      let title = config 
-        ? BeekeeperPlugin.buildConnectionName(config) 
+      let title = config
+        ? `${BeekeeperPlugin.buildConnectionName(config)} - Beekeeper Studio`
         : 'Beekeeper Studio'
-      if (config) {
-        title = title.length > maxLength
-          ? `${title.substring(0, maxLength)}...${suffix}`
-          : `${title}${suffix}`
-      }
       if (context.getters.isTrial && context.getters.isUltimate) {
         const days = context.rootGetters['licenses/licenseDaysLeft']
         title += ` - Free Trial (${window.main.pluralize('day', days, true)} left)`
