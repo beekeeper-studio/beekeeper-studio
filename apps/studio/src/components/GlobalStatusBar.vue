9c9abc53f4bd9e758c7a78f9315fa34eb204ffbb
azmy60 <mazmiagus2003@gmail.com>
2025-04-04 13:13:45 +0700

    make global status bar

---
+++ apps/studio/src/components/GlobalStatusBar.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="global-status-bar">
+    <connection-button />
+    <portal-target name="global-status-bar"></portal-target>
+  </div>
+</template>
+
+<script>
+import { PortalTarget } from 'portal-vue'
+import ConnectionButton from './sidebar/core/ConnectionButton.vue'
+
+export default {
+  components: { PortalTarget, ConnectionButton },
+}
+</script>
