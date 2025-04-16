7255332b3eab8b66b98d26bb3afd479e64538884
azmy60 <mazmiagus2003@gmail.com>
2025-04-16 19:15:11 +0700

    type setup

---
+++ apps/ui-kit/lib/types.ts
@@ -0,0 +1,9 @@
+// Export all types from components
+export * from './components/types';
+export * from './components/table/types';
+export * from './components/data-editor/types';
+export * from './components/sql-text-editor/define';
+export * from './components/entity-list/models';
+
+// Re-export specific utility types
+export * from './utils/entity';
