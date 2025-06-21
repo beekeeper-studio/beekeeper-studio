80db8a5d1dfdd40fa3e6f99e761e12d0a00be6e9
azmy60 <mazmiagus2003@gmail.com>
2025-06-21 15:40:18 +0700

    fix query selection for cm6

--- apps/ui-kit/lib/components/sql-text-editor/v2/extensions/index.ts
+++ apps/ui-kit/lib/components/sql-text-editor/v2/extensions/index.ts
@@ -2,18 +2,29 @@ import { sqlContextComplete, sqlCompletionSource } from "./sqlContextComplete";
 import { sql } from "./customSql";
 import { removeQueryQuotesExtension } from "./removeQueryQuotes";
 import { sqlHighlighter } from "./sqlHighlighter";
+import { querySelection, QuerySelectionChangeParams } from "./querySelection";
+import { Options } from "sql-query-identifier";
 
 export { applyColumnsGetter } from "./sqlContextComplete";
 export type { ColumnsGetter } from "./sqlContextComplete";
 export { applySqlExtension, applyEntities } from "./customSql";
 export { applyDialect } from "./removeQueryQuotes";
+export { QuerySelectionChangeParams };
+
+export type SQLExtensionsConfig = {
+  identiferDialect: Options["dialect"];
+  onQuerySelectionChange: (params: QuerySelectionChangeParams) => void
+}
 
 /**
  * Get all base SQL extensions
  */
-export const extensions = [
-  sql(undefined, sqlCompletionSource),
-  sqlHighlighter,
-  removeQueryQuotesExtension(),
-  sqlContextComplete(),
-];
+export function extensions(config: SQLExtensionsConfig) {
+  return [
+    sql(undefined, sqlCompletionSource),
+    sqlHighlighter,
+    removeQueryQuotesExtension(),
+    sqlContextComplete(),
+    querySelection(config.identiferDialect, config.onQuerySelectionChange),
+  ];
+}
