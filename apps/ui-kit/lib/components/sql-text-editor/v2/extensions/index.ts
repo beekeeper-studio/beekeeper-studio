7fd7503447bff4109f3d0c36a915b8c2cfbe2ae1
azmy60 <mazmiagus2003@gmail.com>
2025-06-22 12:59:10 +0700

    fix editor.selection

--- apps/ui-kit/lib/components/sql-text-editor/v2/extensions/index.ts
+++ apps/ui-kit/lib/components/sql-text-editor/v2/extensions/index.ts
@@ -2,14 +2,15 @@ import { sqlContextComplete, sqlCompletionSource } from "./sqlContextComplete";
 import { sql } from "./customSql";
 import { removeQueryQuotesExtension } from "./removeQueryQuotes";
 import { sqlHighlighter } from "./sqlHighlighter";
-import { querySelection, QuerySelectionChangeParams } from "./querySelection";
+import { querySelection } from "./querySelection";
+import type { QuerySelectionChangeParams } from "./querySelection";
 import { Options } from "sql-query-identifier";
 
 export { applyColumnsGetter } from "./sqlContextComplete";
 export type { ColumnsGetter } from "./sqlContextComplete";
 export { applySqlExtension, applyEntities } from "./customSql";
 export { applyDialect } from "./removeQueryQuotes";
-export { QuerySelectionChangeParams } from "./querySelection";
+export type { QuerySelectionChangeParams };
 
 export type SQLExtensionsConfig = {
   identiferDialect: Options["dialect"];
--- apps/ui-kit/lib/components/sql-text-editor/v2/types.ts
+++ apps/ui-kit/lib/components/sql-text-editor/v2/types.ts
@@ -1,5 +1,5 @@
 import { TextEditorEventMap } from "../../text-editor";
-import { QuerySelectionChangeParams } from "./extensions";
+import type { QuerySelectionChangeParams } from "./extensions";
 
 export interface SqlTextEditorEventMap extends TextEditorEventMap {
   "bks-query-selection-change": QuerySelectionChangeParams;
