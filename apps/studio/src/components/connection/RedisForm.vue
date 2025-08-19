cb1ae1231e3a5812c28927dcccff5fc32316afea
Andrew Pavlov <an.pavlov@mt-innovations.ru>
2025-08-19 21:03:52 +0300

    fix: adhere to common connection form styling

--- apps/studio/src/components/connection/RedisForm.vue
+++ apps/studio/src/components/connection/RedisForm.vue
@@ -1,37 +1,41 @@
 <template>
   <div class="redis-form">
     <!-- Connection Details -->
-    <div class="form-group">
-      <label for="host">Host</label>
-      <masked-input
-        :value="config.host"
-        :privacy-mode="privacyMode"
-        @input="val => config.host = val"
-        :placeholder="'localhost'"
-      />
-    </div>
-
-    <div class="form-group">
-      <label for="port">Port</label>
-      <masked-input
-        :value="config.port"
-        :privacy-mode="privacyMode"
-        :type="'number'"
-        @input="val => config.port = val"
-        :placeholder="'6379'"
-      />
+    <div class="row gutter">
+      <div class="col s9 form-group">
+        <label for="host">Host</label>
+        <masked-input
+          :value="config.host"
+          :privacy-mode="privacyMode"
+          @input="val => config.host = val"
+          :placeholder="'localhost'"
+        />
+      </div>
+      <div class="col s3 form-group">
+        <label for="port">Port</label>
+        <masked-input
+          :value="config.port"
+          :privacy-mode="privacyMode"
+          :type="'number'"
+          @input="val => config.port = val"
+          :placeholder="'6379'"
+        />
+      </div>
     </div>
 
     <!-- Authentication -->
-    <div class="form-group">
+    <div class="form-group form-group-password">
       <label for="password">Password (optional)</label>
       <input
-        id="password"
+        :type="togglePasswordInputType"
         v-model="config.password"
-        type="password"
-        class="form-control"
+        class="password form-control"
         placeholder="password"
       >
+      <i
+        @click.prevent="togglePassword"
+        class="material-icons password-icon"
+      >{{ togglePasswordIcon }}</i>
     </div>
 
     <!-- Redis Specific Options -->
@@ -70,8 +74,24 @@ export default {
       default: false,
     },
   },
+  data() {
+    return {
+      showPassword: false,
+    }
+  },
   computed: {
     ...mapState('settings', ['privacyMode']),
+    togglePasswordIcon() {
+      return this.showPassword ? "visibility_off" : "visibility"
+    },
+    togglePasswordInputType() {
+      return this.showPassword ? "text" : "password"
+    },
+  },
+  methods: {
+    togglePassword() {
+      this.showPassword = !this.showPassword
+    }
   },
   created() {
     // Set default values if not already set
