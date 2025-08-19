7cc3468d5b5ab93f64875ac54fd2cc9965261fe6
Andrew Pavlov <an.pavlov@mt-innovations.ru>
2025-08-19 20:04:23 +0300

    chore: update redis seeds

--- dev/docker_redis/data.sh
+++ dev/docker_redis/data.sh
@@ -15,6 +15,7 @@ redis-cli SET user:1:age "30"
 redis-cli SET user:2:name "Jane Smith"
 redis-cli SET user:2:email "jane.smith@example.com"
 redis-cli SET user:2:age "25"
+redis-cli SET article:1001 "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt."
 
 # Hash examples
 redis-cli HSET user:1:profile name "John Doe" email "john.doe@example.com" age 30 city "New York" occupation "Software Engineer"
