668cffb6c350b46bb3a36b9b8c4b7a8d0e40676e
Mohammad Azmi <mazmiagus2003@gmail.com>
2025-08-25 20:02:24 +0700

    limit query result rows for plugins

--- apps/studio/src/typings/bksConfig.d.ts
+++ apps/studio/src/typings/bksConfig.d.ts
@@ -139,6 +139,9 @@ declare interface IBksConfig {
         "bks-ai-shell": {
             enabled: boolean;
         };
+        config: {
+            queryResultLimit: number;
+        };
     };
     security: {
         disconnectOnIdle: boolean;
