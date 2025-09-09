d075e66072cf3abb1d5b1a3d58d213805242178e
Mohammad Azmi <mazmiagus2003@gmail.com>
2025-09-09 10:38:49 +0700

    add beta tag to redis form

--- apps/studio/src/components/connection/RedisForm.vue
+++ apps/studio/src/components/connection/RedisForm.vue
@@ -1,5 +1,11 @@
 <template>
   <div class="redis-form">
+    <div class="alert alert-warning">
+      <i class="material-icons">warning</i>
+      <span>
+        Redis support is still in beta. Please report any problems on <a href="https://github.com/beekeeper-studio/beekeeper-studio/issues/new/choose">our issue tracker</a>.
+      </span>
+    </div>
     <!-- Connection Details -->
     <div class="row gutter">
       <div class="col s9 form-group">
