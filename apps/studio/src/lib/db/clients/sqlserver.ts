12db163d7658a75463029abe4a0db6a263602f6f
Matthew Rathbone <matthew.rathbone@gmail.com>
2025-10-09 16:50:12 -0500

    remove test error

--- apps/studio/src/lib/db/clients/sqlserver.ts
+++ apps/studio/src/lib/db/clients/sqlserver.ts
@@ -977,7 +977,6 @@ export class SQLServerClient extends BasicDatabaseClient<SQLServerResult> {
 
   async connect(signal?: AbortSignal): Promise<void> {
     await super.connect();
-    throw new Error("login failed for user <token-identified principal> Something something")
 
     this.dbConfig = await this.configDatabase(this.server, this.database, signal)
     this.pool = await new ConnectionPool(this.dbConfig).connect();
