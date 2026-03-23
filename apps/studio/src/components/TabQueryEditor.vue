d5f0e9e0640afb524a2ef9b68c18c06372c07fc5
Day Matchullis <dsm6069@gmail.com>
2026-03-23 12:07:45 -0600

    Fix configuration for params that don't have an explicitly different default

--- apps/studio/src/components/TabQueryEditor.vue
+++ apps/studio/src/components/TabQueryEditor.vue
@@ -749,7 +749,7 @@
           }
           const values = Object.values(this.queryParameterValues) as string[];
           const convertedParams = convertParamsForReplacement(placeholders, values);
-          query = deparameterizeQuery(query, this.dialect, convertedParams, this.$bksConfig.db[this.dialect]?.paramTypes);
+          query = deparameterizeQuery(query, this.dialect, convertedParams, this.paramTypes);
         } catch (ex) {
           log.error("Unable to deparameterize query", ex)
         }
@@ -822,7 +822,7 @@
         if (this.dialect === 'redis') {
           return {};
         }
-        return this.$bksConfig.db[this.dialect]?.paramTypes
+        return this.$bksConfig.db[this.connectionType]?.paramTypes
       },
       identifierDialect() {
         return findSqlQueryIdentifierDialect(this.queryDialect)
@@ -1289,9 +1289,6 @@
 
         this.showKeepAlive = false
         this.maybeCloseWarningNoty();
-        this.tab.isRunning = true
-        this.updateTab();
-        this.running = true
         this.error = null
         this.queryForExecution = rawQuery
         this.results = []
@@ -1328,6 +1325,10 @@
               return;
             }
           }
+
+          this.tab.isRunning = true
+          this.updateTab();
+          this.running = true
 
           const query = this.deparameterizedQuery
           this.$modal.hide(`parameters-modal-${this.tab.id}`)
