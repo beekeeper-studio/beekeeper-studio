2bfb4af8c548182aa7ece788e2e884e7d19376b5
Mohammad Azmi <mohammad.azmi.id@gmail.com>
2026-03-25 18:23:01 +0700

    fix ssh test

--- apps/studio/tests/integration/lib/db/clients/ssh.spec.js
+++ apps/studio/tests/integration/lib/db/clients/ssh.spec.js
@@ -2,7 +2,6 @@ import { DockerComposeEnvironment, Wait } from 'testcontainers'
 import ConnectionProvider from '@commercial/backend/lib/connection-provider';
 import { dbtimeout } from '../../../../lib/db'
 import { TestOrmConnection } from '@tests/lib/TestOrmConnection';
-import { DB_CONFIGS, SshEnvironment } from '@tests/integration/lib/db/clients/ssh/SshEnvironment';
 
 
 
@@ -47,7 +46,6 @@ describe("SSH Tunnel Tests", () => {
       username: 'postgres',
       password: 'example',
       sshEnabled: true,
-      sshMode: 'userpass',
       sshHost: container.getHost(),
       sshPort: container.getMappedPort(2222),
       sshUsername: 'beekeeper',
@@ -86,67 +84,4 @@ describe("SSH Tunnel Tests", () => {
     }
     await TestOrmConnection.disconnect()
   })
-
-  for (const type of Object.keys(DB_CONFIGS)) {
-    describe(type, () => {
-      let sshEnvironment;
-      let sshDatabase;
-
-      beforeAll(async () => {
-        sshEnvironment = new SshEnvironment(type);
-        await sshEnvironment.start();
-      });
-
-      afterAll(async () => {
-        await sshEnvironment?.stop();
-      });
-
-      beforeEach(async () => {
-        sshDatabase = await sshEnvironment.connect();
-      });
-
-      afterEach(async () => {
-        await sshDatabase?.disconnect()
-      });
-
-      it("should work", async () => {
-        await expect(sshDatabase.executeQuery("SELECT 1")).resolves.toBeDefined();
-      })
-
-      it("should detect connection lost", async () => {
-        const fn = jest.fn();
-
-        sshDatabase.connection.on("connection-lost", fn);
-
-        await sshEnvironment.restart();
-
-        // Must run a query to trigger the connection-lost event
-        await expect(sshDatabase.listTables()).rejects.toThrow();
-
-        // Yield to the event loop to allow the "connection-lost" event to fire
-        await new Promise((resolve) => setTimeout(resolve));
-
-        expect(fn).toBeCalled();
-        expect(sshDatabase.connection.isConnected).toBe(false);
-      });
-
-      it("should be able to re-establish connection after losing connection", async () => {
-        const isConnectionLost = new Promise((resolve) => {
-          sshDatabase.connection.once("connection-lost", resolve);
-        });
-
-        await sshEnvironment.restart();
-
-        // Run a query to trigger the connection-lost event
-        await expect(sshDatabase.listTables()).rejects.toThrow();
-
-        // Connection-lost is triggered after running a query
-        await expect(isConnectionLost).resolvesWithin(1000);
-
-        await sshDatabase.connection.connect();
-        expect(sshDatabase.connection.isConnected).toBe(true);
-        await expect(sshDatabase.executeQuery("select 1")).resolves.toBeDefined();
-      })
-    });
-  }
 })
