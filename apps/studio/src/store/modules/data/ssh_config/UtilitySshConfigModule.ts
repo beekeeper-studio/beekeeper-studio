6a2a7010616419938b005eefe308d7d80e24af3b
azmy60 <mohammad.azmi.id@gmail.com>
2026-03-30 16:04:20 +0700

    remove unused syncForConnection

--- apps/studio/src/store/modules/data/ssh_config/UtilitySshConfigModule.ts
+++ apps/studio/src/store/modules/data/ssh_config/UtilitySshConfigModule.ts
@@ -1,10 +1,17 @@
-import { TransportSshConfig, TransportConnectionSshConfig } from "@/common/transport/TransportSshConfig";
-import { DataState, DataStore, mutationsFor, utilActionsFor } from "@/store/modules/data/DataModuleBase";
-import Vue from "vue";
+import { TransportConnectionSshConfig } from "@/common/transport/TransportSshConfig";
+import {
+  DataState,
+  DataStore,
+  mutationsFor,
+  utilActionsFor,
+} from "@/store/modules/data/DataModuleBase";
 
-type State = DataState<TransportConnectionSshConfig>
+type State = DataState<TransportConnectionSshConfig>;
 
-export const UtilSshConfigModule: DataStore<TransportConnectionSshConfig, State> = {
+export const UtilSshConfigModule: DataStore<
+  TransportConnectionSshConfig,
+  State
+> = {
   namespaced: true,
   state: {
     items: [],
@@ -13,63 +20,5 @@ export const UtilSshConfigModule: DataStore<TransportConnectionSshConfig, State>
     pollError: null,
   },
   mutations: mutationsFor<TransportConnectionSshConfig>(),
-  actions: utilActionsFor<TransportConnectionSshConfig>('connectionSshConfig', {
-    /**
-     * Replace all SSH configs for a given connection with the provided ordered list.
-     *
-     * For each new ssh config:
-     *   1. Save it as a standalone ssh_config row (get an id back)
-     *   2. Create a connection_ssh_config join row linking it to the connection with a position
-     *
-     * Then remove the old join rows and any ssh_config rows that are now orphaned.
-     */
-    async syncForConnection(context, { connectionId, sshConfigs }: { connectionId: number, sshConfigs: TransportSshConfig[] }) {
-      // Find existing join rows for this connection
-      const existingJoins: TransportConnectionSshConfig[] = await Vue.prototype.$util.send('appdb/connectionSshConfig/find', {
-        options: { where: { connectionId } }
-      });
-
-      // Save new ssh_config rows and create join rows
-      const newJoins: TransportConnectionSshConfig[] = [];
-      for (let i = 0; i < sshConfigs.length; i++) {
-        const cfg = sshConfigs[i];
-        // Save (or update if it has an id) the standalone ssh_config row
-        const savedConfig: TransportSshConfig = await Vue.prototype.$util.send('appdb/sshConfig/save', { obj: cfg });
-        // Create the join row
-        const join: TransportConnectionSshConfig = await Vue.prototype.$util.send('appdb/connectionSshConfig/save', {
-          obj: {
-            id: null,
-            connectionId,
-            sshConfigId: savedConfig.id,
-            position: i,
-            createdAt: new Date(),
-            updatedAt: new Date(),
-            version: null,
-          }
-        });
-        join.sshConfig = savedConfig;
-        newJoins.push(join);
-      }
-
-      // Remove old join rows
-      if (existingJoins.length > 0) {
-        const orphanedSshConfigIds = existingJoins.map((j) => j.sshConfigId);
-        await Vue.prototype.$util.send('appdb/connectionSshConfig/remove', { obj: existingJoins });
-
-        // Delete ssh_config rows that are now orphaned (not referenced by any other connection)
-        for (const sshConfigId of orphanedSshConfigIds) {
-          const remaining = await Vue.prototype.$util.send('appdb/connectionSshConfig/count', {
-            options: { where: { sshConfigId } }
-          });
-          if (remaining === 0) {
-            await Vue.prototype.$util.send('appdb/sshConfig/remove', { obj: { id: sshConfigId } });
-          }
-        }
-      }
-
-      context.commit('remove', existingJoins);
-      context.commit('upsert', newJoins);
-      return newJoins;
-    }
-  }),
-}
+  actions: utilActionsFor<TransportConnectionSshConfig>("connectionSshConfig"),
+};
