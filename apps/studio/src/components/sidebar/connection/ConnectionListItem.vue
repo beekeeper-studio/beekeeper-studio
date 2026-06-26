8e5095fa36726f13008203faf92ebd161f2621b3
Mohammad Azmi <mohammad.azmi.id@gmail.com>
2026-06-26 17:13:42 +0700

    connection list item ui

--- apps/studio/src/components/sidebar/connection/ConnectionListItem.vue
+++ apps/studio/src/components/sidebar/connection/ConnectionListItem.vue
@@ -21,27 +21,15 @@
             @cancel="rename = false"
           />
         </div>
+        <div class="subtitle">{{ connectionTypeName }}</div>
         <div class="subtitle">
-          <span
-            class="bastion"
-            v-if="displayConfig.sshBastionHost && !privacyMode"
-          >
-            <span class="truncate">{{ displayConfig.sshBastionHost }}</span>&nbsp;>&nbsp;
-          </span>
-          <span
-            class="ssh"
-            v-if="displayConfig.sshHost && !privacyMode"
-          >
-            <span class="truncate">{{ displayConfig.sshHost }}</span>&nbsp;>&nbsp;
-          </span>
-          <span class="connection">
-            <span>
-              {{ privacyMode ? '******' : subtitleSimple }}
-            </span>
-          </span>
+          <span>{{ privacyMode ? "******" : subtitleSimple }}</span>
+          <template v-if="displayConfig.sshEnabled && !privacyMode">
+            <span class="dot" />
+            <span>SSH</span>
+          </template>
         </div>
       </div>
-      <span class="badge"><span>{{ displayConfig.connectionType }}</span></span>
       <span
         v-if="!isRecentList"
         class="actions"
@@ -80,6 +68,7 @@ import { mapGetters, mapState } from 'vuex'
 import { isUltimateType } from '@/common/interfaces/IConnection'
 import { AppEvent } from '@/common/AppEvent'
 import EditableText from '@/components/common/EditableText.vue'
+import { ConnectionTypes } from "@/lib/db/types";
 
 export default {
   components: { EditableText },
@@ -144,6 +133,12 @@ export default {
       }
 
       return 'Url'
+    },
+    connectionTypeName() {
+      const type = ConnectionTypes.find(
+        (t) => t.value === this.displayConfig.connectionType
+      );
+      return type ? type.name : this.displayConfig.connectionType;
     },
     subtitleSimple() {
       if (this.isRecentList) {
@@ -334,6 +329,11 @@ export default {
 }
 </script>
 <style lang="scss" scoped>
+.list-item .list-item-btn .connection-label {
+  height: auto;
+  align-self: stretch;
+}
+
 .list-item .list-item-btn .connection-title {
   min-width: 0;
 
@@ -341,10 +341,32 @@ export default {
     position: relative;
     width: 100%;
     overflow: visible;
+    line-height: 1;
   }
 
   .editable-text {
     width: 100%;
+  }
+
+  .subtitle {
+    line-height: 1.4;
+    overflow: hidden;
+    white-space: nowrap;
+    text-overflow: ellipsis;
+
+    .badge {
+      margin-left: 0.25rem;
+    }
+
+    .dot {
+      border-radius: 9999px;
+      min-width: initial;
+      width: 0.25rem;
+      height: 0.25rem;
+      margin-inline: 0.5rem;
+      align-self: center;
+      background-color: var(--text-lighter);
+    }
   }
 }
 </style>
