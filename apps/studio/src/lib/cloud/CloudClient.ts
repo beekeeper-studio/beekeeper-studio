7144aab5a56e940e8d8aad415b735bcb90df7427
Matthew Rathbone <matthew.rathbone@gmail.com>
2026-08-05 10:45:17 -0500

    ngrok for testing

--- apps/studio/src/lib/cloud/CloudClient.ts
+++ apps/studio/src/lib/cloud/CloudClient.ts
@@ -47,6 +47,9 @@ export interface CloudClientOptions {
 const staticAxios = (baseUrl) => axios.create({
   baseURL: baseUrl,
   timeout: 5000,
+  headers: {
+    "ngrok-skip-browser-warning": true
+  },
   transformRequest: [snakeCaseData, ...defaultTransformRequest],
   transformResponse: [...defaultTransformResponse, camelCaseData],
   validateStatus: (status) => status < 500
@@ -92,6 +95,7 @@ export class CloudClient {
         token: options.token,
         app: options.app,
         clientVersion: options.clientVersion,
+        "ngrok-skip-browser-warning": true
       },
       validateStatus: (status) => status < 500
     })
