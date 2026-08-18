50587d6338f4326a32cf441183056389f714a5c8
azmy60 <mohammad.azmi.id@gmail.com>
2026-08-18 14:03:29 +0700

    add updated by status to query editor

--- apps/studio/src/components/TabQueryEditor.vue
+++ apps/studio/src/components/TabQueryEditor.vue
@@ -91,104 +91,104 @@
         class="toolbar text-right"
         ref="toolbar"
       >
-        <div class="actions" v-if="canManageTransactions">
-          <transition name="fade-swap">
-            <x-buttons
-              id="commit-mode"
-              class="selectbutton"
-              v-if="!hasActiveTransaction"
-            >
-              <x-button
-                :toggled="!isManualCommit"
-                @click.prevent="toggleCommitMode('auto')"
-                v-tooltip="getCommitModeVTooltip({
-                  title: 'Auto commit mode',
-                  description: 'This is the way it works by default. No need to worry about it.',
-                })"
+        <div class="actions secondary-actions">
+          <div v-if="canManageTransactions">
+            <transition name="fade-swap">
+              <x-buttons
+                id="commit-mode"
+                class="selectbutton"
+                v-if="!hasActiveTransaction"
               >
-                <span class="togglebutton-content">
-                  {{ !isManualCommit ? 'Auto Commit' : 'Auto' }}
-                </span>
-              </x-button>
-              <x-button
-                :toggled="isManualCommit"
-                @click.prevent="toggleCommitMode('manual')"
-                v-tooltip="getCommitModeVTooltip({
-                  title: 'Manual commit mode',
-                  description: 'Write actions will require you to manually commit your changes',
-                  learnMoreLink: 'https://docs.beekeeperstudio.io/user_guide/sql_editor/manual-transaction-management',
-                })"
+                <x-button
+                  :toggled="!isManualCommit"
+                  @click.prevent="toggleCommitMode('auto')"
+                  v-tooltip="getCommitModeVTooltip({
+                    title: 'Auto commit mode',
+                    description: 'This is the way it works by default. No need to worry about it.',
+                  })"
+                >
+                  <span class="togglebutton-content">
+                    {{ !isManualCommit ? 'Auto Commit' : 'Auto' }}
+                  </span>
+                </x-button>
+                <x-button
+                  :toggled="isManualCommit"
+                  @click.prevent="toggleCommitMode('manual')"
+                  v-tooltip="getCommitModeVTooltip({
+                    title: 'Manual commit mode',
+                    description: 'Write actions will require you to manually commit your changes',
+                    learnMoreLink: 'https://docs.beekeeperstudio.io/user_guide/sql_editor/manual-transaction-management',
+                  })"
+                >
+                  <span class="togglebutton-content">
+                    {{ isManualCommit ? 'Manual Commit' : 'Manual' }}
+                  </span>
+                </x-button>
+              </x-buttons>
+            </transition>
+            <transition name="fade-swap">
+              <div
+                v-if="hasActiveTransaction"
+                class="transaction-indicator"
+                v-tooltip="{
+                  ...getCommitModeVTooltip({
+                    title: `<i class='material-icons'>commit</i><span>Transaction active</span>`,
+                    description: 'Once committed or rolled back, it will be deactivated.',
+                    learnMoreLink: 'https://docs.beekeeperstudio.io/user_guide/sql_editor/manual-transaction-management',
+                    className: 'transaction-active',
+                    show: showTransactionActiveTooltip,
+                    onClose() {
+                      showTransactionActiveTooltip = false
+                    },
+                  }),
+                }"
               >
-                <span class="togglebutton-content">
-                  {{ isManualCommit ? 'Manual Commit' : 'Manual' }}
-                </span>
+                <i class="material-icons">commit</i>
+                <span>Transaction active</span>
+              </div>
+            </transition>
+          </div>
+
+          <div v-if="canManageTransactions && isManualCommit" class="btn-group">
+            <x-buttons v-show="!hasActiveTransaction">
+              <x-button
+                @click.prevent="manualBegin"
+                class="btn btn-flat btn-small"
+              >
+                Begin
               </x-button>
             </x-buttons>
-          </transition>
-          <transition name="fade-swap">
-            <div
-              v-if="hasActiveTransaction"
-              class="transaction-indicator"
-              v-tooltip="{
-                ...getCommitModeVTooltip({
-                  title: `<i class='material-icons'>commit</i><span>Transaction active</span>`,
-                  description: 'Once committed or rolled back, it will be deactivated.',
-                  learnMoreLink: 'https://docs.beekeeperstudio.io/user_guide/sql_editor/manual-transaction-management',
-                  className: 'transaction-active',
-                  show: showTransactionActiveTooltip,
-                  onClose() {
-                    showTransactionActiveTooltip = false
-                  },
-                }),
-              }"
-            >
-              <i class="material-icons">commit</i>
-              <span>Transaction active</span>
-            </div>
-          </transition>
+
+            <x-buttons v-show="showKeepAlive">
+              <x-button
+                @click.prevent="keepAliveTransaction"
+                class="btn btn-flat btn-small"
+              >
+                <x-label>Keep Alive</x-label>
+              </x-button>
+            </x-buttons>
+            <x-buttons>
+              <x-button
+                @click.prevent="manualCommit"
+                class="btn btn-flat btn-small"
+                :disabled="!hasActiveTransaction"
+              >
+                <x-label>Commit</x-label>
+              </x-button>
+            </x-buttons>
+            <x-buttons>
+              <x-button
+                @click.prevent="manualRollback"
+                class="btn btn-flat btn-small"
+                :disabled="!hasActiveTransaction"
+              >
+                <x-label>Rollback</x-label>
+              </x-button>
+            </x-buttons>
+          </div>
         </div>
 
-        <div v-if="canManageTransactions && isManualCommit" class="actions btn-group">
-          <x-buttons v-show="!hasActiveTransaction">
-            <x-button
-              @click.prevent="manualBegin"
-              class="btn btn-flat btn-small"
-            >
-              Begin
-            </x-button>
-          </x-buttons>
-
-          <x-buttons v-show="showKeepAlive">
-            <x-button
-              @click.prevent="keepAliveTransaction"
-              class="btn btn-flat btn-small"
-            >
-              <x-label>Keep Alive</x-label>
-            </x-button>
-          </x-buttons>
-          <x-buttons>
-            <x-button
-              @click.prevent="manualCommit"
-              class="btn btn-flat btn-small"
-              :disabled="!hasActiveTransaction"
-            >
-              <x-label>Commit</x-label>
-            </x-button>
-          </x-buttons>
-          <x-buttons>
-            <x-button
-              @click.prevent="manualRollback"
-              class="btn btn-flat btn-small"
-              :disabled="!hasActiveTransaction"
-            >
-              <x-label>Rollback</x-label>
-            </x-button>
-          </x-buttons>
-        </div>
-
-        <div class="editor-help expand" />
-        <div class="expand" />
-        <div class="actions btn-group">
+        <div class="actions primary-actions btn-group">
           <x-button
             v-if="showDryRun"
             class="btn btn-flat btn-small dry-run-btn"
@@ -206,6 +206,17 @@
               v-model="dryRun"
             >
           </x-button>
+          <div class="history-btn-wrapper">
+            <x-button
+              v-if="queryId"
+              @click.prevent="viewEditHistory"
+              class="btn btn-flat btn-small history-btn"
+              v-tooltip="updatedTooltip"
+            >
+              <i class="material-icons">history</i>
+              <span class="label">{{ updatedText }}</span>
+            </x-button>
+          </div>
           <x-button
             v-if="aiShellAvailable"
             @click.prevent="askAi"
@@ -561,6 +572,7 @@
   import _ from 'lodash'
   import Split from 'split.js'
   import Noty from 'noty'
+  import dateFormat from 'dateformat'
   import { mapActions, mapGetters, mapState } from 'vuex'
   import { identify } from 'sql-query-identifier'
 
@@ -610,6 +622,7 @@ import { KeybindingPath } from '@/common/bksConfig/BksConfigProvider'
     },
     data() {
       return {
+        latestAudit: null,
         results: [],
         running: false,
         runningCount: 1,
@@ -702,8 +715,46 @@ import { KeybindingPath } from '@/common/bksConfig/BksConfigProvider'
           { event: AppEvent.openQueryEditHistory, handler: this.handleOpenQueryEditHistory },
         ];
       },
-      savedQuery(): ISavedQuery | undefined {
-        return this.savedQueries.find((q) => q.id === this.tab.queryId);
+      updatedByName() {
+        return this.latestAudit?.user?.name;
+      },
+      updatedAt() {
+        if (!this.latestAudit) {
+          return null;
+        }
+
+        // the cloud api sends float seconds since epoch, appdb sends a Date
+        if (typeof this.latestAudit.createdAt === "number") {
+          return new Date(this.latestAudit.createdAt * 1000);
+        }
+
+        return this.latestAudit.createdAt;
+      },
+      updatedTooltip() {
+        if (!this.updatedAt) {
+          return;
+        }
+
+        const time = dateFormat(this.updatedAt, "d mmm yyyy HH:MM:ss");
+
+        if (this.isCloud && this.updatedByName) {
+          return `Updated by ${this.updatedByName} at ${time}`;
+        }
+
+        return `Updated at ${time}`;
+      },
+      updatedText() {
+        if (!this.updatedAt) {
+          return;
+        }
+
+        const ago = `${this.$bks.timeAgo(this.updatedAt, "mini")} ago`;
+
+        if (this.isCloud && this.updatedByName) {
+          return `${this.updatedByName} ${ago}`;
+        }
+
+        return ago;
       },
       readOnly() {
         if (this.tab.isLoading) {
@@ -712,7 +763,7 @@ import { KeybindingPath } from '@/common/bksConfig/BksConfigProvider'
         if (this.remoteDeleted) {
           return true;
         }
-        if (this.isCloud && this.savedQuery && !this.savedQuery.canWrite) {
+        if (this.isCloud && this.query.id && !this.query.canWrite) {
           return true;
         }
         return false;
@@ -746,7 +797,10 @@ import { KeybindingPath } from '@/common/bksConfig/BksConfigProvider'
         return this.storeInitialized && this.tab.queryId && this.queryDeleted
       },
       query() {
-        return this.fullQuery ?? this.blankQuery
+        return this.fullQuery || this.savedQueries.find((q) => q.id === this.tab.queryId) || this.blankQuery
+      },
+      queryId() {
+        return this.query.id
       },
       queryTitle() {
         return this.query?.title
@@ -1011,6 +1065,12 @@ import { KeybindingPath } from '@/common/bksConfig/BksConfigProvider'
       }
     },
     watch: {
+      queryId: {
+        immediate: true,
+        handler() {
+          this.loadLatestAudit();
+        },
+      },
       selectedResult() {
         this.editingResult = false
       },
@@ -1088,7 +1148,24 @@ import { KeybindingPath } from '@/common/bksConfig/BksConfigProvider'
     methods: {
       ...mapActions({
         reloadQuery: "data/queries/reload",
+        listQueryAudits: "data/queryAudits/list",
       }),
+      async loadLatestAudit() {
+        if (!this.queryId) {
+          return;
+        }
+
+        try {
+          const audits = await this.listQueryAudits({
+            queryId: this.queryId,
+            limit: 1,
+          });
+          this.latestAudit = audits[0] ?? null;
+        } catch (e) {
+          log.error("failed loading the latest query audit", e);
+          this.latestAudit = null;
+        }
+      },
       updateTab() {
         this.$emit('update-tab', this.tab)
       },
@@ -2040,6 +2117,8 @@ import { KeybindingPath } from '@/common/bksConfig/BksConfigProvider'
 
             if (!query) return;
 
+            this.fullQuery = query;
+
             if (this.tab.title !== query.title) {
               this.tab.title = query.title;
               this.updateTab();
@@ -2272,6 +2351,70 @@ import { KeybindingPath } from '@/common/bksConfig/BksConfigProvider'
   .ask-ai .material-icons {
     font-size: 1rem;
     margin-right: 0.25rem;
+  }
+
+  .toolbar x-button {
+    white-space: nowrap;
+  }
+
+  .query-editor .toolbar .actions.secondary-actions {
+    margin: 0;
+    overflow-x: auto;
+
+    &::-webkit-scrollbar {
+      height: 1px;
+    }
+
+    .btn-group {
+      display: flex;
+      margin-left: 0.25rem;
+      margin-right: 0;
+    }
+  }
+
+  .query-editor .toolbar .actions.primary-actions {
+    flex-grow: 1;
+    justify-content: flex-end;
+    margin: 0;
+  }
+
+  .history-btn-wrapper {
+    container: query-editor-history-btn-wrapper / inline-size;
+    flex-grow: 1;
+    min-width: 2rem;
+  }
+
+  .btn.history-btn {
+    gap: 0.25rem;
+    background-color: transparent;
+    box-shadow: none;
+    padding-inline: 0.15rem;
+
+    .label {
+      display: none;
+      font-weight: 400;
+      min-width: 0;
+      overflow: hidden;
+      text-overflow: ellipsis;
+      white-space: nowrap;
+    }
+
+    &:not(:hover) {
+      .material-icons {
+        color: var(--text-lighter);
+      }
+
+      .label {
+        color: var(--text-light);
+      }
+    }
+  }
+
+  // 76px fits the icon plus the longest time string, "11mo ago"
+  @container query-editor-history-btn-wrapper (min-width: 76px) {
+    .btn.history-btn .label {
+      display: inline;
+    }
   }
 </style>
 
