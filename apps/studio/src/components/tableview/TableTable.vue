a00ed469807db22d5491ba0be1c2dff9f2a9c91d
Matthew Rathbone <matthew.rathbone@gmail.com>
2026-08-19 16:45:12 -0500

    Statusbar column hidden icon styles were broken, now fixed

--- apps/studio/src/components/tableview/TableTable.vue
+++ apps/studio/src/components/tableview/TableTable.vue
@@ -181,19 +181,16 @@
           </x-buttons>
         </template>
         <span
-          v-else
-          class="hidden-column-count bks-tooltip-wrapper statusbar-item hoverable"
+          v-else-if="hiddenColumnCount"
+          class="hidden-column-count statusbar-item hoverable"
+          v-tooltip="hiddenColumnMessage"
         >
           <a
             tabindex="0"
             @click.prevent="showColumnFilterModal"
-            v-if="hiddenColumnCount"
           >
             <i class="material-icons">visibility_off</i>
           </a>
-          <div class="bks-tooltip bks-tooltip-top-center">
-            <span>{{ hiddenColumnMessage }}</span>
-          </div>
         </span>
 
         <template v-if="!editable">
