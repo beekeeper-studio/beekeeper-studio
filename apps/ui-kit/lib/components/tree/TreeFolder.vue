d82fb9cab69414dfcc83e4fa44a95904ead0bb14
Mohammad Azmi <mohammad.azmi.id@gmail.com>
2026-09-07 22:22:58 +0700

    make connection interface themeable

--- apps/ui-kit/lib/components/tree/TreeFolder.vue
+++ apps/ui-kit/lib/components/tree/TreeFolder.vue
@@ -44,24 +44,14 @@ export default Vue.extend({
   display: flex;
   align-items: center;
   border: none;
-  background-color: transparent;
   padding: 0;
   width: 100%;
   border-radius: 4px;
   cursor: pointer;
-  color: rgb(from var(--bks-theme-base) r g b / 77%);
   font-size: 1rem;
   height: 1.75rem;
   padding-left: calc(var(--depth) * 1rem);
   padding-right: 0.8rem;
-
-  &:hover {
-    background-color: rgb(from var(--bks-theme-base) r g b / 3.5%);
-
-    .expand-icon {
-      color: var(--bks-text-dark);
-    }
-  }
 }
 
 .name {
@@ -74,13 +64,13 @@ export default Vue.extend({
 }
 
 .material-icons.folder-icon {
-  color: var(--bks-text-lighter);
+  color: var(--icon-fg);
   font-size: 1rem;
   margin-left: 0.25rem;
 }
 
 .material-icons.expand-icon {
-  color: var(--bks-text-lighter);
+  color: var(--icon-fg);
   font-size: 1.25rem;
 }
 </style>
