9a8b094b8d23ecb049fa6ed3a0450530e00a2bc7
Day Matchullis <dsm6069@gmail.com>
2026-09-11 12:46:01 -0600

    remove stale fixes for odbc

--- .github/scripts/install-build-deps.sh
+++ .github/scripts/install-build-deps.sh
@@ -3,5 +3,5 @@
 set -euxo pipefail
 
 sudo apt update
-sudo apt install -y flatpak flatpak-builder rpm libarchive-tools unixodbc-dev
+sudo apt install -y flatpak flatpak-builder rpm libarchive-tools
 flatpak --user remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
