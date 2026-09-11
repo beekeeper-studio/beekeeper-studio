8a2d2702609d0a46ab30709e2449952fee0e6e49
Day Matchullis <dsm6069@gmail.com>
2026-09-10 22:13:17 -0600

    add missing build deps for linux arm

--- .github/scripts/install-build-deps.sh
+++ .github/scripts/install-build-deps.sh
@@ -3,5 +3,5 @@
 set -euxo pipefail
 
 sudo apt update
-sudo apt install -y flatpak flatpak-builder rpm libarchive-tools
+sudo apt install -y flatpak flatpak-builder rpm libarchive-tools unixodbc-dev
 flatpak --user remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
